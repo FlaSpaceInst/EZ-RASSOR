03183dfc90e6d11496e70bef513e4a602e477e34
Tiger Sachse <tiger@sachse.io>
2018-11-09 23:07:45 +0000

    finished with raspi setup script

--- scripts/pi_setup.sh
+++ scripts/pi_setup.sh
@@ -1,6 +1,28 @@
-# WIP -TS
+# A collection of functions that automatically install and prepare ROS.
+# For use on a fresh Raspberry Pi. These functions are tuned for
+# Raspbian Stretch and ROS Kinetic and so if you wish to use
+# distributions other than these you may need to modify this script.
 
+# This script can be run using this command:
+#       $ bash pi_setup.sh [--flag]
+
+# Flags allow you to execute different parts of this script:
+#   --prerequisites
+#       Install and configure the prerequisites for ROS on the Raspberry Pi.
+#   --download
+#       Download ROS packages into a new Catkin workspace.
+#   --build
+#       Build ROS packages in a Catkin workspace.
+# These flags can be chained together, and they will execute in order.
+# If you provide no flags, all of the functions will be run in the correct
+# order (recommended).
+
+# Written by Tiger Sachse for the EZ-RASSOR project.
+
+SHELL="bash"
+SHELL_RC="$HOME/.bashrc"
 WORKSPACE="$HOME/workspace"
+INSTALL_SPACE="/opt/ros/kinetic"
 
 # Configure the prerequisites for ROS.
 function setup_prerequisites {
@@ -26,22 +48,44 @@ function setup_prerequisites {
 # Download some ROS packages to get us started.
 function download_packages {
 
-    # Create a workspace.
+    # Create a Catkin workspace.
     rm -rf $WORKSPACE
     mkdir -p $WORKSPACE
     cd $WORKSPACE
 
-    # Install the core ROS packages and communication libraries.
-    rosinstall_generator ros_comm --rosdistro kinetic --deps --wet-only --tar > \
-        kinetic-ros_comm-wet.rosinstall
+    # Install the core ROS packages and communication libraries. This section
+    # skips downloading Assimp because it has issues during compilation and
+    # is unneeded anyway.
+    rosinstall_generator ros_comm --rosdistro kinetic --deps --wet-only \
+        --exclude collada_parser collada_urdf --tar > kinetic-ros_comm-wet.rosinstall
     wstool init src kinetic-ros_comm-wet.rosinstall
+
+    # Resolve dependencies.
+    rosdep install -y --from-paths src --ignore-src --rosdistro kinetic \
+        -r --os=debian:stretch
+
+    cd -
+}
+
+# Build the packages located in the Catkin workspace.
+function build_packages {
+    cd $WORKSPACE
+
+    sudo ./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release \
+        --install-space $INSTALL_SPACE -j2 --quiet
+
+    # Source the setup script for the appropriate shell.
+    source $INSTALL_SPACE/setup.$SHELL
+    echo "source $INSTALL_SPACE/setup.$SHELL" >> $SHELL_RC
+
+    cd -
 }
 
 # Run every part of this script.
 function run_full_installation {
-    #setup_prerequisites
-    #download_packages
-    echo "running all"
+    setup_prerequisites
+    download_packages
+    build_packages
 }
 
 # Main entry point to the script. If no arguments are provided, run everything.
@@ -55,8 +99,11 @@ else
             --prerequisites)
                 setup_prerequisites
                 ;;
-            --packages)
+            --download)
                 download_packages
+                ;;
+            --build)
+                build_packages
                 ;;
         esac
     done
