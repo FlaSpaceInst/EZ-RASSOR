f6fb3c7236e2671633b50e5f6e593da7ab4fced0
Tiger <tgsachse@gmail.com>
2018-11-16 21:51:10 +0000

    remove excessive copying with magical symlinks!

--- operations.sh
+++ operations.sh
@@ -3,39 +3,71 @@ PACKAGES_DIR="packages"
 WORK_SPACE="$HOME/.workspace"
 SOURCE_SPACE="$WORK_SPACE/src"
 
+# Make the main workspace.
 function make_workspace {
     mkdir -p $SOURCE_SPACE
+
     cd $WORK_SPACE
     catkin_make
+    cd -
 }
 
-function clean {
-    rm -rf $WORK_SPACE
+function build_packages {
+    echo ""
 }
 
-function copy_packages {
+function run_ros_graph {
+    echo ""
+}
+
+# Create a new ROS package.
+function create_new_package {
     if [ ! -d $SOURCE_SPACE ]; then
         make_workspace
     fi
-    
-    for PACKAGE in $PACKAGES_DIR/*; do
-        cp -r $PACKAGE $SOURCE_SPACE/
-    done
+
+    cd $PACKAGES_DIR
+
+    # Create a new catkin package with all the arguments
+    # passed to this function.
+    catkin_create_pkg "$@"
+
+    # Make a few modifications to the new package structure.
+    mkdir "$1/scripts"
+    rm -rf "$1/include" "$1/src"
+
+    # Create a symlink in the main workspace so that catkin
+    # can build this new package.
+    ln -s "$PWD/$1" "$SOURCE_SPACE/$1"
+
+    cd -
+}
+
+# Relink all packages to the ROS workspace.
+function relink_packages {
+    if [ ! -d $SOURCE_SPACE ]; then
+        make_workspace
+    fi
 }
 
 for ARGUMENT in "$@"; do
     case $ARGUMENT in
         --install)
             bash $SCRIPTS_DIR/install_ros.sh
-            ;;
-        --copy)
-            copy_packages
+            make_workspace
             ;;
         --build)
+            build_packages
             ;;
         --run)
+            run_ros_graph
             ;;
-        --clean)
+        --new)
+            shift
+            create_new_package "$@"
+            ;;
+        --relink)
+            relink_packages
             ;;
     esac
 done
