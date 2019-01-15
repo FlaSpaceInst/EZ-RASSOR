415e509aede3c98d15734fe194634e9bebf8a685
Tiger Sachse <tgsachse@gmail.com>
2019-01-15 15:05:53 -0500

    preliminary installation and catkin setup update

--- ezrassor.sh
+++ ezrassor.sh
@@ -1,72 +1,64 @@
 SCRIPTS_DIR="scripts"
 PACKAGES_DIR="packages"
-WORK_SPACE="$HOME/.workspace"
-SOURCE_SPACE="$WORK_SPACE/src"
+WORKSPACE_DIR="$HOME/.workspace"
+SOURCE_DIR="$WORKSPACE_DIR/src"
 
-# Make the main workspace.
-function make_workspace {
-    mkdir -p $SOURCE_SPACE
+install_software() {
+    case $1 in
+        ros)
+            bash $SCRIPTS_DIR/install_ros.sh
+            ;;
+        devtools)
+            sudo apt install -y python-rosinstall python-rosinstall-generator \
+                python-wstool build-essential
+            ;;
+    esac
+}
 
-    cd $WORK_SPACE
+setup_catkin() {
+    mkdir -p $SOURCE_DIR
+
+    cd $WORKSPACE_DIR
     catkin_make
-    cd -
 }
 
-function build_packages {
-    echo ""
-}
+# Create a new ROS package in source control.
+# Arguments are:
+#   superpackage package [dependencies]
+create_package() {
 
-function run_ros_graph {
-    echo ""
-}
+    # Make the superpackage, if necessary.
+    mkdir -p $PACKAGES_DIR/$1
 
-# Create a new ROS package.
-function create_new_package {
-    if [ ! -d $SOURCE_SPACE ]; then
-        make_workspace
-    fi
-
-    cd $PACKAGES_DIR
+    cd $PACKAGES_DIR/$1
 
     # Create a new catkin package with all the arguments
     # passed to this function.
-    catkin_create_pkg "$@"
+    catkin_create_pkg "${@:2}"
 
     # Add a scripts directory to the new package structure.
-    mkdir "$1/scripts"
+    mkdir "$2/scripts"
 
     # Create a symlink in the main workspace so that catkin
     # can build this new package.
-    ln -s "$PWD/$1" "$SOURCE_SPACE/$1"
-
-    cd -
+    ln -s "$PWD/$2" "$SOURCE_DIR/$2"
 }
 
 # Relink all packages to the ROS workspace.
 function relink_packages {
-    if [ ! -d $SOURCE_SPACE ]; then
+    if [ ! -d $SOURCE_DIR ]; then
         make_workspace
     fi
 }
 
-for ARGUMENT in "$@"; do
-    case $ARGUMENT in
-        --install)
-            bash $SCRIPTS_DIR/install_ros.sh
-            make_workspace
-            ;;
-        --build)
-            build_packages
-            ;;
-        --run)
-            run_ros_graph
-            ;;
-        --new)
-            shift
-            create_new_package "$@"
-            ;;
-        --relink)
-            relink_packages
-            ;;
-    esac
-done
+case $1 in
+    --install)
+        install_software $2
+        ;;
+    --setup-catkin)
+        setup_catkin
+        ;;
+    --create-package)
+        create_package "${@:2}"
+        ;;
+esac
