7764e8bc589400b25b465e38fa04f9e3f1f2c9d7
Harrison Black <Harrison.W.Black@gmail.com>
2019-01-27 15:09:27 -0500

    Added controller support to Gazebo sim for driving.

--- packages/ezrc/ezrc_core/scripts/wheels.py
+++ packages/ezrc/ezrc_core/scripts/wheels.py
@@ -1,48 +1,95 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import Int16
+from std_msgs.msg import Int16, Float64
+
+MASK = 0b111100000000
+
+# /ez_rassor/left_wheel_back_velocity_controller/command
+pubLF = rospy.Publisher('/ez_rassor/left_wheel_front_velocity_controller/command', Float64, queue_size = 10)
+pubLB = rospy.Publisher('/ez_rassor/left_wheel_back_velocity_controller/command', Float64, queue_size = 10)
+pubRF = rospy.Publisher('/ez_rassor/right_wheel_front_velocity_controller/command', Float64, queue_size = 10)
+pubRB = rospy.Publisher('/ez_rassor/right_wheel_back_velocity_controller/command', Float64, queue_size = 10)
 
 
-def callback(data):
+def get_movements(integer, mask):
+    """Decode a bitstring to reveal the movement commands for this node."""
 
-	data_in = data.data
+    # Use a mask to remove unnecessary bits.
+    result = integer & mask
+
+    # Shift the result so the commands are the 4 least significant bits. The
+    # amount of shifting is determined by the mask.
+    while mask % 2 == 0:
+        result >>= 1
+        mask >>= 1
+
+    # Return the 4 least significant bits as boolean values.
+    return (
+        result & 0b1000 != 0,
+        result & 0b100 != 0,
+        result & 0b10 != 0,
+        result & 0b1 != 0
+    )
+
+
+def wheel_movement_callback(instruction):
+	# print("callback")
+	# data_in = data.data
 	
-	mask = 0b111100000000
-	data_in &= mask
-	data_in >>= 8
-	data_string = "Wheels:\t {0:04b}".format(data_in)
+	# # mask = 0b111100000000
+	# data_in &= mask
+	# data_in >>= 8
+	# data_string = "Wheels:\t {0:04b}".format(data_in)
 
-	if data_in == 0b1000:
-		data_string = data_string + " -> Drive Forward"
+	drive_forward, drive_reverse, turn_left, turn_right = get_movements(instruction.data, MASK)
 
-		# Insert motor functions
+	velocity = 10
 
-	elif data_in == 0b0100:
-	    data_string = data_string + " -> Reverse"
+	if drive_forward:
+		# data_string = data_string + " -> Drive Forward"
+		pubLF.publish(velocity)
+		pubLB.publish(velocity)
+		pubRF.publish(velocity)
+		pubRB.publish(velocity)
 
-	    # Insert motor functions
+	elif drive_reverse:
+		# data_string = data_string + " -> Reverse"
+		pubLF.publish(-velocity)
+		pubLB.publish(-velocity)
+		pubRF.publish(-velocity)
+		pubRB.publish(-velocity)
 
-	elif data_in == 0b0010:
-	    data_string = data_string + " -> Turn Left"
+	elif turn_left:
+		# data_string = data_string + " -> Turn Left"
+		pubLF.publish(-velocity)
+		pubLB.publish(-velocity)
+		pubRF.publish(velocity)
+		pubRB.publish(velocity)
 
-	    # Insert motor functions
-
-	elif data_in == 0b0001:
-	    data_string = data_string + " -> Turn Right"
-
-	    # Insert motor functions
+	elif turn_right:
+		# data_string = data_string + " -> Turn Right"
+		pubLF.publish(velocity)
+		pubLB.publish(velocity)
+		pubRF.publish(-velocity)
+		pubRB.publish(-velocity)
 
 	else:
-	    data_string = data_string + " -> Stop"
+		# data_string = data_string + " -> Stop"
+		# Halt motor functions
+		pubLF.publish(0)
+		pubLB.publish(0)
+		pubRF.publish(0)
+		pubRB.publish(0)
 
-	    # Halt motor functions
 
-	print(data_string)
+
+	# print(data_string)
 
 def main():
 
-	rospy.init_node('ez_arms', anonymous = True)
-	rospy.Subscriber('ezmain_topic', Int16, callback)
+	print("Wheel node started")
+	rospy.init_node('ez_wheels', anonymous = True)
+	rospy.Subscriber('ez_main_topic', Int16, wheel_movement_callback)
 	rospy.spin()
 
 if __name__ == '__main__':
