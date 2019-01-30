be7b9b380448d8bed1411d857944d59af3d8cf95
Tiger <tgsachse@gmail.com>
2019-01-30 15:16:22 +0000

    refactor wheel control into class

--- packages/ezrc/ezrc_moving_parts/scripts/wheels_node.py
+++ packages/ezrc/ezrc_moving_parts/scripts/wheels_node.py
@@ -15,19 +15,21 @@ import RPi.GPIO as GPIO
 
 
 # Change these constants if you want to break this node. :)
-SPEED = 3000
-FREQUENCY = 60
+SPEED = 2000
 FRONT_SERVO_PIN = 8
+GPIO_MODE = GPIO.BCM
+DRIVER_FREQUENCY = 60
 MASK = 0b111100000000
-LEFT_WHEEL = {
-    "pwm" : 5,
-    "pins" : (17, 18),
-    "frequency" : 400,
+
+LEFT_WHEEL_SPECS = {
+    "pwm_pin" : 5,
+    "gpio_pins" : (17, 18),
+    "servo_frequency" : 450,
 }
-RIGHT_WHEEL = {
-    "pwm" : 4,
-    "pins" : (27, 22),
-    "frequency" : 550,
+RIGHT_WHEEL_SPECS = {
+    "pwm_pin" : 4,
+    "gpio_pins" : (22, 27),
+    "servo_frequency" : 525,
 }
 
 NODE_NAME = "wheels_node"
@@ -35,14 +37,57 @@ TOPIC_NAME = "ezmain_topic"
 MESSAGE_FORMAT = "EZRC ({0}.py): %s.".format(NODE_NAME)
 
 
+class Wheel:
+    """"""
+    FORWARD = True
+    BACKWARD = False
+
+    def __init__(self,
+                 pwm_pin,
+                 gpio_pins,
+                 speed,
+                 driver,
+                 servo_frequency,
+                 gpio_mode=GPIO.BCM,
+                 enable_gpio_warnings=False):
+        """"""
+        self.pwm_pin = pwm_pin
+        self.gpio_pins = gpio_pins
+        self.speed = speed
+        self.driver = driver
+        self.servo_frequency = servo_frequency
+
+        GPIO.setmode(gpio_mode)
+        GPIO.setwarnings(enable_gpio_warnings)
+
+        for pin in self.gpio_pins:
+            GPIO.setup(pin, GPIO.OUT)
+
+    def set_direction(self, direction):
+        """"""
+        if direction == Wheel.FORWARD:
+            GPIO.output(self.gpio_pins[0], GPIO.HIGH)
+            GPIO.output(self.gpio_pins[1], GPIO.LOW)
+        elif direction == Wheel.BACKWARD:
+            GPIO.output(self.gpio_pins[0], GPIO.LOW)
+            GPIO.output(self.gpio_pins[1], GPIO.HIGH)
+
+    def start(self):
+        """"""
+        driver.set_pwm(self.pwm_pin, 0, self.speed)
+
+    def stop(self):
+        """"""
+        driver.set_pwm(self.pwm_pin, 0, 0)
+        GPIO.output(self.gpio_pins[0], GPIO.LOW)
+        GPIO.output(self.gpio_pins[1], GPIO.LOW)
+
+
 def rotate_wheels(nibble_queue,
                   left_wheel,
                   right_wheel,
                   front_servo_pin,
-                  speed,
-                  driver_frequency,
-                  gpio_mode=GPIO.BCM,
-                  enable_gpio_warnings=False):
+                  driver):
     """Move the arms of the EZRC.
     
     The arms are controlled by sending boolean 4-tuples to this function via
@@ -51,28 +96,16 @@ def rotate_wheels(nibble_queue,
     topic) can occur simultaneously. 
     """
 
-    GPIO.setmode(gpio_mode)
-    GPIO.setwarnings(enable_gpio_warnings)
-
-    for pin in left_wheel["pins"]:
-        GPIO.setup(pin, GPIO.OUT)
-    for pin in right_wheel["pins"]:
-        GPIO.setup(pin, GPIO.OUT)
-
-    # Create a new driver for the PCA9685 board.
-    driver = Adafruit_PCA9685.PCA9685()
-    driver.set_pwm_freq(driver_frequency)
-
     # These movement booleans tell the main function loop whether to raise or
     # lower a particular arm.
-    left_wheel_forward = False
-    left_wheel_backward = False
-    right_wheel_forward = False
-    right_wheel_backward = False
+    turn_left = False
+    turn_right = False
+    drive_forward = False
+    drive_backward = False
 
     # Initialize both arms to be vertical.
-    front_servo_pin_hearth = (left_wheel["frequency"]
-                              + right_wheel["frequency"]) // 2
+    front_servo_pin_hearth = (left_wheel.servo_frequency
+                              + right_wheel.servo_frequency) // 2
     driver.set_pwm(front_servo_pin, 0, front_servo_pin_hearth)
 
     while True:
@@ -86,54 +119,84 @@ def rotate_wheels(nibble_queue,
             if nibble == None:
                 break
             else:
-                left_wheel_forward, left_wheel_backward = nibble[:2]
-                right_wheel_forward, right_wheel_backward = nibble[2:]
+                drive_forward, drive_backward, turn_left, turn_right = nibble
 
-                if left_wheel_forward:
-                    GPIO.output(left_wheel["pins"][0], GPIO.HIGH)
-                    GPIO.output(left_wheel["pins"][1], GPIO.LOW)
-                    driver.set_pwm(front_servo_pin, 0, left_wheel["frequency"])
-                    driver.set_pwm(left_wheel["pwm"], 0, speed)
+                if drive_forward:
+                    left_wheel.set_direction(Wheel.FORWARD)
+                    right_wheel.set_direction(Wheel.FORWARD)
+                    driver.set_pwm(front_servo_pin, 0, front_servo_pin_hearth)
+                    left_wheel.start()
+                    right_wheel.start()
+
+                elif drive_backward:
+                    left_wheel.set_direction(Wheel.BACKWARD)
+                    right_wheel.set_direction(Wheel.BACKWARD)
+                    driver.set_pwm(front_servo_pin, 0, front_servo_pin_hearth)
+                    left_wheel.start()
+                    right_wheel.start()
+
+                elif turn_left:
+                    left_wheel.set_direction(Wheel.FORWARD)
+                    right_wheel.set_direction(Wheel.BACKWARD)
+                    #driver.set_pwm(front_servo_pin, 0, left_wheel.get_pwm_pin())
+
+                else:
+                    driver.set_pwm(front_servo_pin, 0, front_servo_pin_hearth)
+                    left_wheel.stop()
+                    right_wheel.stop()
 
         except Queue.Empty:
             pass
 
     # Clean up after the loop is broken. 
     driver.set_pwm(front_servo_pin, 0, front_servo_pin_hearth)
-    driver.set_pwm(left_wheel["pwm"], 0, 0)
-    driver.set_pwm(right_wheel["pwm"], 0, 0)
+    left_wheel.stop()
+    right_wheel.stop()
 
 
 def print_status(nibble, message_format):
     """Print status information based on the provided nibble."""
-    left_wheel_forward, left_wheel_backward = nibble[:2]
-    right_wheel_forward, right_wheel_backward = nibble[2:]
+    drive_forward, drive_backward, turn_left, turn_right = nibble
 
     if not any(nibble):
-        print message_format % "Stopping both wheels"
+        print message_format % "Stopping the car"
     else:
-        if left_wheel_forward:
-            print message_format % "Rotating left wheel forward"
-        if left_wheel_backward:
-            print message_format % "Rotating left wheel backward"
-        if right_wheel_forward:
-            print message_format % "Rotating right wheel forward"
-        if right_wheel_backward:
-            print message_format % "Rotating right wheel backward"
+        if drive_forward:
+            print message_format % "Driving the car forward"
+        if drive_backward:
+            print message_format % "Driving the car backward"
+        if turn_left:
+            print message_format % "Turning the car left"
+        if turn_right:
+            print message_format % "Turning the car right"
 
 
 # Main entry point to this node.
 try:
+    # Create a new driver for the PCA9685 board.
+    driver = Adafruit_PCA9685.PCA9685()
+    driver.set_pwm_freq(DRIVER_FREQUENCY)
+
+    left_wheel = Wheel(LEFT_WHEEL_SPECS["pwm_pin"],
+                       LEFT_WHEEL_SPECS["gpio_pins"],
+                       SPEED,
+                       driver,
+                       LEFT_WHEEL_SPECS["servo_frequency"])
+    
+    right_wheel = Wheel(RIGHT_WHEEL_SPECS["pwm_pin"],
+                        RIGHT_WHEEL_SPECS["gpio_pins"],
+                        SPEED,
+                        driver,
+                        RIGHT_WHEEL_SPECS["servo_frequency"])
 
     # Create a queue and process to rotate the wheels.
     nibble_queue = multiprocessing.Queue()
     movement_process = multiprocessing.Process(target=rotate_wheels,
                                                args=(nibble_queue,
-                                                     LEFT_WHEEL,
-                                                     RIGHT_WHEEL,
+                                                     left_wheel,
+                                                     right_wheel,
                                                      FRONT_SERVO_PIN,
-                                                     SPEED,
-                                                     FREQUENCY))
+                                                     driver))
     movement_process.start()
 
     # Initialize this node as a subscriber.
