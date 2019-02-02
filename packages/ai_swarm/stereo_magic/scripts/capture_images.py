e4cc6999d05f9f81e7b92a3cb438902d12cc3dd1
Cameron Taylor <cetaylor758903@knights.ucf.edu>
2019-02-01 21:44:39 -0500

    Final CDR Update

--- packages/ai_swarm/stereo_magic/scripts/capture_images.py
+++ packages/ai_swarm/stereo_magic/scripts/capture_images.py
@@ -13,7 +13,7 @@ import time
 import scipy
 
 #change it to your own global path to workspace
-GLOBAL_PATH = '/home/chico55/.workspace'
+GLOBAL_PATH = '/home/cameron/.workspace'
 #cv2.namedWindow("window", cv2.WINDOW_AUTOSIZE) 
 class Rectify:
 
