dd2df140fbab393b5e6e56533e5551ca78cb21aa
Cameron Taylor <cetaylor758903@knights.ucf.edu>
2019-02-02 10:53:37 -0500

    Added new world and launch script for everything AI - CDR - Demo

---
+++ scripts/ai_demo.sh
@@ -0,0 +1,37 @@
+cd $1
+source "devel/setup.bash"
+
+# ROS master and Gazebo world launch.
+xterm -e roslaunch ez_rassor_gazebo ai_demo.launch &
+
+sleep 6
+
+
+# Spawn control nodes for ez_rassor hardware
+rosrun ezrc_core wheels.py &
+rosrun ezrc_core arms.py &
+rosrun ezrc_core drums.py &
+
+sleep 2
+
+# Spawn SLAM Viewer
+rosrun lsd_slam_viewer viewer &
+
+sleep 2
+
+# Spawn SLAM Core
+rosrun lsd_slam_core live_slam \
+    /image:=/ez_rassor/front_camera/left/image_raw \
+    /camera_info:=/ez_rassor/front_camera/left/camera_info & 
+
+sleep 2
+
+
+echo "Starting AI script in 3..."
+sleep 1
+echo "Starting AI script in 2..."
+sleep 1
+echo "Starting AI script in 1..."
+sleep 1
+
+rosrun ai_control ai_control.py
