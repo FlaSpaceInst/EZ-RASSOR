71e7dfcd52749a2ceb158613e3bb4c5c7a137fe1
Cameron Taylor <cetaylorfl@gmail.com>
2019-02-19 17:01:03 -0500

    Reworked AI Control and introduced digging and proper arm angle adjustment

--- packages/ai_swarm/disparity/scripts/distance_map.py
+++ packages/ai_swarm/disparity/scripts/distance_map.py
@@ -7,6 +7,7 @@ import cv2
 from cv_bridge import CvBridge, CvBridgeError 
 from stereo_msgs.msg import DisparityImage
 import std_msgs
+import matplotlib.pyplot as plt
 
 # Written by Tyler Duncan
 # The following code assumes Gazebo to be running and the EZ-RASSOR executing the following scripts:
@@ -18,61 +19,64 @@ import std_msgs
 #   rosrun image_view stereo_view stereo:=ez_rassor/front_camera image:=image_rect
 #
 #**************************************************************************************************
-#	These scripts utilize the Disparity map generation provided by ROS out of the box using the 
-#	stereo_image_proc package.  So far this has returned the fastest disparity map generation.  
+#    These scripts utilize the Disparity map generation provided by ROS out of the box using the 
+#    stereo_image_proc package.  So far this has returned the fastest disparity map generation.  
 
 
 
 #==================================================================================================
 # TODO: I need to figure out how to get the disparityImage message in such a way 
-# 		that I can use the focal length value, baseline value, and disparity matrix
-#		values to calculate a distance matrix. 
-#		
-#		Z[i][j] = fT / d[i][j], where
-#		
-#		-	Z[i][j] = the distance to pixel (i, j) 
-#		-	f = focal distance 
-#		-	T = baseline 
-#		-	d = disparity value of pixel (i, j). 
+#         that I can use the focal length value, baseline value, and disparity matrix
+#        values to calculate a distance matrix. 
+#        
+#        Z[i][j] = fT / d[i][j], where
+#        
+#        -    Z[i][j] = the distance to pixel (i, j) 
+#        -    f = focal distance 
+#        -    T = baseline 
+#        -    d = disparity value of pixel (i, j). 
 #
-#		Numpy.reciprocal will invert all entries in the disparity matrix and then we simply need to
-#		multiply by fT on each entry to get depth information. 
+#        Numpy.reciprocal will invert all entries in the disparity matrix and then we simply need to
+#        multiply by fT on each entry to get depth information. 
 #===================================================================================================
 
 
 def callback(data):
-	# Testing Subscription. 
-	# rospy.loginfo(rospy.get_caller_id() + "The camera focal length is " + str(data.f))
+    # Testing Subscription. 
+    # rospy.loginfo(rospy.get_caller_id() + "The camera focal length is " + str(data.f))
 
 
-	bridge = CvBridge()
-	cv_image = bridge.imgmsg_to_cv2(data.image, "8UC1").astype("float64")
-	
-	depth_mat = np.multiply((data.f * data.T), np.reciprocal(cv_image.astype("float64")))
-	max_arr = depth_mat.min(0)
-	rows, cols = depth_mat.shape
-
-	# print(max_arr)
-	
-	div_mat = np.split(depth_mat, 2)
+    bridge = CvBridge()
+    cv_image = bridge.imgmsg_to_cv2(data.image, "8UC1").astype("float64")
+    
+    depth_mat = np.multiply((data.f * data.T), np.reciprocal(cv_image.astype("float64")))
+    print(size(depth_mat))
+    plt.matshow(depth_mat)
+    plt.savefig('image.png')
 
 
+    max_arr = depth_mat.min(0)
+    rows, cols = depth_mat.shape
 
-	print(div_mat[1].min(0))
+    
+    div_mat = np.split(depth_mat, 2)
 
-	# if max_left.mean > max_right.mean:
-	# 	print("MOVE RIGHT!")
-	# elif max_left.mean < max_right.mean:
-	# 	print("MOVE LEFT!") 
+
+
+
+    # if max_left.mean > max_right.mean:
+    #     print("MOVE RIGHT!")
+    # elif max_left.mean < max_right.mean:
+    #     print("MOVE LEFT!") 
 
 def depth_estimator():
 
-	rospy.init_node('depth_estimator')
-	rospy.Subscriber("/ez_rassor/front_camera/disparity", DisparityImage, callback)
-	rospy.spin()
+    rospy.init_node('depth_estimator')
+    rospy.Subscriber("/ez_rassor/front_camera/disparity", DisparityImage, callback)
+    rospy.spin()
 
 if __name__ == "__main__":
-	try:
-		depth_estimator()
-	except ROSInterruptException:
-		pass
+    try:
+        depth_estimator()
+    except ROSInterruptException:
+        pass
