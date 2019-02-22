41d0a1cea718ef28df4ca917d7d51da0fbb31223
Tiger <tgsachse@gmail.com>
2019-02-22 19:18:32 +0000

    #115 true tank turning

--- packages/ezrc/ezrc_control/scripts/wheels_driver.py
+++ packages/ezrc/ezrc_control/scripts/wheels_driver.py
@@ -16,21 +16,14 @@ import RPi.GPIO as GPIO
 
 # Change these constants if you want to break this node. :)
 SPEED = 2000
-FRONT_SERVO_PIN = 8
 GPIO_MODE = GPIO.BCM
 DRIVER_FREQUENCY = 60
 MASK = 0b111100000000
 
-LEFT_WHEEL_SPECS = {
-    "pwm_pin" : 5,
-    "gpio_pins" : (17, 18),
-    "servo_frequency" : 450,
-}
-RIGHT_WHEEL_SPECS = {
-    "pwm_pin" : 4,
-    "gpio_pins" : (22, 27),
-    "servo_frequency" : 325,
-}
+LEFT_WHEEL_PWM = 5
+RIGHT_WHEEL_PWM = 4
+LEFT_WHEEL_GPIO = (17, 18)
+RIGHT_WHEEL_GPIO = (22, 27)
 
 NODE_NAME = "wheels_driver"
 MOVEMENT_TOGGLES_TOPIC = "/ezrassor/movement_toggles"
@@ -47,7 +40,6 @@ class Wheel:
                  gpio_pins,
                  speed,
                  driver,
-                 servo_frequency,
                  gpio_mode=GPIO.BCM,
                  enable_gpio_warnings=False):
         """Initialize a wheel with some GPIO and PWM settings."""
@@ -55,24 +47,20 @@ class Wheel:
         self.gpio_pins = gpio_pins
         self.speed = speed
         self.driver = driver
-        self.servo_frequency = servo_frequency
 
         GPIO.setmode(gpio_mode)
         GPIO.setwarnings(enable_gpio_warnings)
         for pin in self.gpio_pins:
             GPIO.setup(pin, GPIO.OUT)
 
-    def set_direction(self, direction):
-        """Set the direction that this wheel rotates."""
+    def start(self, direction):
+        """Start rotating in the given direction!"""
         if direction == Wheel.FORWARD:
             GPIO.output(self.gpio_pins[0], GPIO.HIGH)
             GPIO.output(self.gpio_pins[1], GPIO.LOW)
         elif direction == Wheel.BACKWARD:
             GPIO.output(self.gpio_pins[0], GPIO.LOW)
             GPIO.output(self.gpio_pins[1], GPIO.HIGH)
-
-    def start(self):#combine with set direction in next issue
-        """Start rotating!"""
         driver.set_pwm(self.pwm_pin, 0, self.speed)
 
     def stop(self):
@@ -85,7 +73,6 @@ class Wheel:
 def rotate_wheels(toggle_queue,
                   left_wheel,
                   right_wheel,
-                  front_servo_pin,
                   driver):
     """Move the wheels of the EZRC.
     
@@ -96,16 +83,11 @@ def rotate_wheels(toggle_queue,
     """
 
     # These rotation booleans tell the main function loop what direction the
-    # EZRC should attempt to move.
-    turn_left = False
-    turn_right = False
-    drive_forward = False
-    drive_backward = False
-
-    # Initialize the front servo to point straight forward.
-    front_servo_pin_hearth = (left_wheel.servo_frequency
-                              + right_wheel.servo_frequency) // 2
-    driver.set_pwm(front_servo_pin, 0, front_servo_pin_hearth)
+    # EZRC's wheels should attempt to move.
+    left_forward = False
+    left_backward = False
+    right_forward = False
+    right_backward = False
 
     while True:
 
@@ -119,65 +101,44 @@ def rotate_wheels(toggle_queue,
             if toggles == None:
                 break
             else:
-                drive_forward, drive_backward, turn_left, turn_right = toggles
+                left_forward, left_backward, right_forward, right_backward = toggles
 
-                if drive_forward:
-                    left_wheel.set_direction(Wheel.FORWARD)
-                    right_wheel.set_direction(Wheel.FORWARD)
-                    driver.set_pwm(front_servo_pin, 0, front_servo_pin_hearth)
-                    left_wheel.start()
-                    right_wheel.start()
-
-                elif drive_backward:
-                    left_wheel.set_direction(Wheel.BACKWARD)
-                    right_wheel.set_direction(Wheel.BACKWARD)
-                    driver.set_pwm(front_servo_pin, 0, front_servo_pin_hearth)
-                    left_wheel.start()
-                    right_wheel.start()
-
-                elif turn_left:
-                    left_wheel.set_direction(Wheel.FORWARD)
-                    right_wheel.set_direction(Wheel.BACKWARD)
-                    driver.set_pwm(front_servo_pin, 0, left_wheel.pwm_pin)
-                    left_wheel.start()
-                    right_wheel.start()
-
-                elif turn_right:
-                    left_wheel.set_direction(Wheel.BACKWARD)
-                    right_wheel.set_direction(Wheel.FORWARD)
-                    driver.set_pwm(front_servo_pin, 0, right_wheel.pwm_pin)
-                    left_wheel.start()
-                    right_wheel.start()
-
+                if left_forward:
+                    left_wheel.start(Wheel.FORWARD)
+                elif left_backward:
+                    left_wheel.start(Wheel.BACKWARD)
                 else:
-                    driver.set_pwm(front_servo_pin, 0, front_servo_pin_hearth)
                     left_wheel.stop()
-                    right_wheel.stop()
 
+                if right_forward:
+                    right_wheel.start(Wheel.FORWARD)
+                elif right_backward:
+                    right_wheel.start(Wheel.BACKWARD)
+                else:
+                    right_wheel.stop()
         except Queue.Empty:
             pass
 
     # Clean up and stop the wheels after the loop is broken. 
-    driver.set_pwm(front_servo_pin, 0, front_servo_pin_hearth)
     left_wheel.stop()
     right_wheel.stop()
 
 
 def print_status(toggles, message_format):
     """Print status information based on the provided toggles."""
-    drive_forward, drive_backward, turn_left, turn_right = toggles
+    left_forward, left_backward, right_forward, right_backward = toggles
 
     if not any(toggles):
         print message_format % "Stopping the car"
     else:
-        if drive_forward:
-            print message_format % "Driving the car forward"
-        if drive_backward:
-            print message_format % "Driving the car backward"
-        if turn_left:
-            print message_format % "Turning the car left"
-        if turn_right:
-            print message_format % "Turning the car right"
+        if left_forward:
+            print message_format % "Driving left side forward"
+        if left_backward:
+            print message_format % "Driving left side backward"
+        if right_forward:
+            print message_format % "Driving right side forward"
+        if right_backward:
+            print message_format % "Driving right side backward"
 
 
 # Main entry point to this node.
@@ -187,19 +148,8 @@ try:
     driver = Adafruit_PCA9685.PCA9685()
     driver.set_pwm_freq(DRIVER_FREQUENCY)
 
-    # Create new Wheel objects using the left and right wheel specs defined at
-    # the top of this script.
-    left_wheel = Wheel(LEFT_WHEEL_SPECS["pwm_pin"],
-                       LEFT_WHEEL_SPECS["gpio_pins"],
-                       SPEED,
-                       driver,
-                       LEFT_WHEEL_SPECS["servo_frequency"])
-    
-    right_wheel = Wheel(RIGHT_WHEEL_SPECS["pwm_pin"],
-                        RIGHT_WHEEL_SPECS["gpio_pins"],
-                        SPEED,
-                        driver,
-                        RIGHT_WHEEL_SPECS["servo_frequency"])
+    left_wheel = Wheel(LEFT_WHEEL_PWM, LEFT_WHEEL_GPIO, SPEED, driver)
+    right_wheel = Wheel(RIGHT_WHEEL_PWM, RIGHT_WHEEL_GPIO, SPEED, driver)
 
     # Create a queue and process to rotate the wheels.
     toggle_queue = multiprocessing.Queue()
@@ -207,12 +157,11 @@ try:
                                                args=(toggle_queue,
                                                      left_wheel,
                                                      right_wheel,
-                                                     FRONT_SERVO_PIN,
                                                      driver))
     movement_process.start()
 
     # Initialize this node as a subscriber.
-    rospy.init_node(NODE_NAME, anonymous=True)
+    rospy.init_node(NODE_NAME)
     rospy.Subscriber(MOVEMENT_TOGGLES_TOPIC,
                      std_msgs.msg.Int16,
                      callback=utilities.enqueue_toggles,
