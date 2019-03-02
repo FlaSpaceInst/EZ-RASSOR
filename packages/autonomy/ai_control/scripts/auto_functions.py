b6467b14230aacfc2a4982c470e0d9743c6baa75
Cameron Taylor <cetaylorfl@gmail.com>
2019-03-02 17:18:36 -0500

    Further restructing for more modular approach and better control of variables - Some namespace adjusting for simplicity.

--- packages/autonomy/ai_control/scripts/auto_functions.py
+++ packages/autonomy/ai_control/scripts/auto_functions.py
@@ -1,47 +1,47 @@
 #!/usr/bin/env python
 import rospy
 
-def auto_drive(world_state, rospy_config):
+def auto_drive(world_state, ros_util):
     """ Travel forward in a straight line. Avoid obstacles while maintaining heading. """
     
     while world_state.auto_function_command != 0:
         while(world_state.state_flags['warning_flag'] == 1):
-            rospy_config.command_pub.publish(rospy_config.commands['right'])
-            rospy_config.rate.sleep()
+            ros_util.command_pub.publish(ros_util.commands['right'])
+            ros_util.rate.sleep()
         while(world_state.state_flags['warning_flag'] == 2):
-            rospy_config.command_pub.publish(rospy_config.commands['left'])
-            rospy_config.rate.sleep()
+            ros_util.command_pub.publish(ros_util.commands['left'])
+            ros_util.rate.sleep()
         
-        rospy_config.command_pub.publish(rospy_config.commands['forward'])
-        rospy_config.rate.sleep()
+        ros_util.command_pub.publish(ros_util.commands['forward'])
+        ros_util.rate.sleep()
 
-    rospy_config.command_pub.publish(rospy_config.commands['null'])
+    ros_util.command_pub.publish(ros_util.commands['null'])
 
-def auto_drive_location(world_state, rospy_config, location):
+def auto_drive_location(world_state, ros_util, location):
     """ Navigate to location. Avoid obstacles while moving toward location. """
     pass
 
 
-def auto_dig(world_state, rospy_config, duration):
+def auto_dig(world_state, ros_util, duration):
     """ Rotate both drums inward and drive forward for duration time in seconds. """
 
     while world_state.auto_function_command != 0:
         while(world_state.state_flags['warning_flag_front'] == 1):
-            rospy_config.command_pub.publish(rospy_config.commands['right'])
-            rospy_config.rate.sleep()
+            ros_util.command_pub.publish(ros_util.commands['right'])
+            ros_util.rate.sleep()
         while(world_state.state_flags['warning_flag_front'] == 2):
-            rospy_config.command_pub.publish(rospy_config.commands['left'])
-            rospy_config.rate.sleep()
+            ros_util.command_pub.publish(ros_util.commands['left'])
+            ros_util.rate.sleep()
         
-        rospy_config.command_pub.publish(rospy_config.commands['forward'] | rospy_config.commands['front_dig'] | rospy_config.commands['back_dig'])
-        rospy_config.rate.sleep()
+        ros_util.command_pub.publish(ros_util.commands['forward'] | ros_util.commands['front_dig'] | ros_util.commands['back_dig'])
+        ros_util.rate.sleep()
 
-    rospy_config.command_pub.publish(rospy_config.commands['null'])
+    ros_util.command_pub.publish(ros_util.commands['null'])
 
-def auto_dock(world_state, rospy_config):
+def auto_dock(world_state, ros_util):
     """ Dock with the hopper. """
     pass
 
-def auto_self_right(world_state, rospy_config):
+def auto_self_right(world_state, ros_util):
     """  """
     pass
--- packages/autonomy/ai_control/scripts/utility_functions.py
+++ packages/autonomy/ai_control/scripts/utility_functions.py
@@ -1,32 +1,36 @@
 #!/usr/bin/env python
 import rospy
 
-def set_front_arm_angle(world_state, target_angle):
+def set_front_arm_angle(world_state, ros_util, target_angle):
     """ Set front arm to absolute angle target_angle in radians. """
 
     if target_angle > world_state.state_flags['front_arm_angle']:
         while(target_angle > world_state.state_flags['front_arm_angle']):
-            command_pub.publish(commands['front_arm_up'])
-            rate.sleep()
+            ros_util.command_pub.publish(ros_util.commands['front_arm_up'])
+            ros_util.rate.sleep()
     else:
         while(target_angle < world_state.state_flags['front_arm_angle']):
-            command_pub.publish(commands['front_arm_down'])
-            rate.sleep()
+            ros_util.command_pub.publish(ros_util.commands['front_arm_down'])
+            ros_util.rate.sleep()
 
     
-    command_pub.publish(commands['null'])
+    ros_util.command_pub.publish(ros_util.commands['null'])
 
 
-def set_back_arm_angle(world_state, target_angle):
+def set_back_arm_angle(world_state, ros_util, target_angle):
     """ Set back arm to absolute angle target_angle in radians. """
 
     if target_angle > world_state.state_flags['back_arm_angle']:
         while(target_angle > world_state.state_flags['back_arm_angle']):
-            command_pub.publish(commands['back_arm_up'])
-            rate.sleep()
+            ros_util.command_pub.publish(ros_util.commands['back_arm_up'])
+            ros_util.rate.sleep()
     else:
         while(target_angle < world_state.state_flags['back_arm_angle']):
-            command_pub.publish(commands['back_arm_down'])
-            rate.sleep()
+            ros_util.command_pub.publish(ros_util.commands['back_arm_down'])
+            ros_util.rate.sleep()
 
-    command_pub.publish(commands['null'])
+    ros_util.command_pub.publish(ros_util.commands['null'])
+
+def initial_check(world_state, ros_util):
+    """  """
+    pass
