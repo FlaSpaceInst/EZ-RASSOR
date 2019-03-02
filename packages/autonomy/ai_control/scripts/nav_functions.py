d5fe1b7d1a76c85bfe8fb2781c5b358c0e5d002d
Cameron Taylor <cetaylorfl@gmail.com>
2019-03-02 17:11:06 -0500

    Further restructing for more modular approach and better control of variables

--- packages/autonomy/ai_control/scripts/nav_functions.py
+++ packages/autonomy/ai_control/scripts/nav_functions.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import Int8, Int16, String
-from nav_msgs.msg import Odometry
-from gazebo_msgs.msg import LinkStates
-from sensor_msgs.msg import JointState
-from ai_control.msg import ObstacleDetection
-from WorldState import WorldState
-import time
 import math
 
 
