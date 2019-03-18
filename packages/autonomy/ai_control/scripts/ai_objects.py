e6e0bcdbe60d9576b665971b4d3c4a6c440dbabb
Cameron Taylor <cetaylor758903@knights.ucf.edu>
2019-03-18 18:05:47 -0400

    Trying to make launch files

--- packages/autonomy/ai_control/scripts/ai_objects.py
+++ packages/autonomy/ai_control/scripts/ai_objects.py
@@ -48,7 +48,7 @@ class ROSUtility():
 
         self. auto_function_command = 0
 
-        self.commands = {'forward' : 0b100000000000, 'reverse' : 0b010000000000, 'left' : 0b001000000000, 'right' : 0b000100000000, 
+        self.commands = {'forward' : 0b101000000000, 'reverse' : 0b010100000000, 'left' : 0b011000000000, 'right' : 0b100100000000, 
                 'front_arm_up' : 0b000010000000, 'front_arm_down' : 0b000001000000, 'back_arm_up' : 0b000000100000, 'back_arm_down' : 0b000000010000,
                 'front_dig' : 0b000000001000, 'front_dump' : 0b000000000100, 'back_dig' : 0b000000000010, 'back_dump' : 0b000000000001,
                 'arms_up' : 0b000010100000, 'arms_down' : 0b000001010000, 'null': 0b000000000000}
