7918cccccfc41972cf400f8c3710dede97f5764f
Chris <guitartaliaferro@yahoo.com>
2019-04-05 12:51:40 -0400

    CleanuP

--- packages/interfaces/ez_gui-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pandaslap/Desktop/NASA-E-RASSOR-Team/packages/interfaces/ez_gui
-# Build directory: /home/pandaslap/Desktop/NASA-E-RASSOR-Team/packages/interfaces/ez_gui-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
