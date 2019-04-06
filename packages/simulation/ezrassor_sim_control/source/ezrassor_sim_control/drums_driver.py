c6cc4b6a757e99e3e4e051e82149c9315becab62
Cameron Taylor <cetaylor758903@knights.ucf.edu>
2019-04-06 18:44:56 -0400

    new sim drivers

--- packages/simulation/ezrassor_sim_control/source/ezrassor_sim_control/drums_driver.py
+++ packages/simulation/ezrassor_sim_control/source/ezrassor_sim_control/drums_driver.py
@@ -1,68 +1,51 @@
+"""A ROS node that moves the drums on the EZRC.
+
+Written by Harrison Black and Tiger Sachse.
+Part of the EZ-RASSOR suite of software.
+"""
 import rospy
-from std_msgs.msg import Int16, Float64
+import std_msgs
+from std_msgs.msg import Float32, Float64
 
 NODE = "drums"
-TOPIC = "/movement_toggles"
-MASK = 0b000000001111
-
+FRONT_TOPIC = "/front_drum_instructions"
+BACK_TOPIC = "/back_drum_instructions"
+MAX_DRUM_SPEED = 1
 
 # /ezrassor/drum_back_velocity_controller/command
-# /ezrassor/drum_front_velocity_controller/command
-pub_FD = rospy.Publisher('/drum_front_velocity_controller/command', Float64, queue_size = 10)
-pub_BD = rospy.Publisher('/drum_back_velocity_controller/command', Float64, queue_size = 10)
+pub_FA = rospy.Publisher('/drum_front_velocity_controller/command', 
+                         Float64, 
+                         queue_size = 10)
+pub_BA = rospy.Publisher('/drum_back_velocity_controller/command', 
+                         Float64, 
+                         queue_size = 10)
 
-def get_movements(integer, mask):
-    """Decode a bitstring to reveal the movement commands for this node."""
+def handle_front_drum_movements(data):
+    """Move the front drum of the EZRC per 
+        the commands encoded in the instruction.
+    """
+    pub_FA.publish(data.data*MAX_DRUM_SPEED)
 
-    # Use a mask to remove unnecessary bits.
-    result = integer & mask
+def handle_back_drum_movements(data):
+    """Move the back drum of the EZRC per 
+        the commands encoded in the instruction.
+    """
+    pub_BA.publish(data.data*MAX_DRUM_SPEED)
 
-    # Shift the result so the commands are the 4 least significant bits. The
-    # amount of shifting is determined by the mask.
-    while mask % 2 == 0:
-        result >>= 1
-        mask >>= 1
-
-    # Return the 4 least significant bits as boolean values.
-    return (
-        result & 0b1000 != 0,
-        result & 0b100 != 0,
-        result & 0b10 != 0,
-        result & 0b1 != 0
-    )
-
-def drum_movement_callback(instruction):
-
-    front_dig, front_dump, back_dig, back_dump = get_movements(instruction.data, MASK)
-    drum_speed = 5
-
-    # Front drums
-    if front_dig:
-        pub_FD.publish(drum_speed)
-
-    elif front_dump:
-        pub_FD.publish(-drum_speed)
-
-    else:
-        pub_FD.publish(0)
-
-    # Back Drums
-    if back_dig:
-        pub_BD.publish(-drum_speed)
-
-    elif back_dump:
-        pub_BD.publish(drum_speed)
-
-    else:
-        pub_BD.publish(0)
 
 def start_node():
+    # Main entry point to the node.
     try:
-        print("Drums node started")
-        rospy.init_node(NODE, anonymous = True)
-        # rospy.Subscriber('ez_main_topic', Int16, drum_movement_callback)
-        rospy.Subscriber(TOPIC, Int16, drum_movement_callback)
+        rospy.init_node(NODE, anonymous=True)
+        rospy.Subscriber(FRONT_TOPIC, Float32, handle_front_drum_movements)
+        rospy.Subscriber(BACK_TOPIC, Float32, handle_back_drum_movements)
         rospy.spin()
-
     except rospy.ROSInterruptException:
         pass
+
+
+
+
+
+pub_FD = rospy.Publisher('/drum_front_velocity_controller/command', Float64, queue_size = 10)
+pub_BD = rospy.Publisher('/drum_back_velocity_controller/command', Float64, queue_size = 10)
