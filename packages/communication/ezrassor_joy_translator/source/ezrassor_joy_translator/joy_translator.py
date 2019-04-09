cc6434046f89893fcd78f5399d8c1df6a8f06535
Harrison Black <Harrison.W.Black@gmail.com>
2019-04-08 21:55:07 -0400

    Joy translator and launch rewritten

--- packages/communication/ezrassor_joy_translator/source/ezrassor_joy_translator/joy_translator.py
+++ packages/communication/ezrassor_joy_translator/source/ezrassor_joy_translator/joy_translator.py
@@ -4,27 +4,45 @@
 Written by Harrison Black.
 """
 import rospy
-from std_msgs.msg import Int8, Int32
+from std_msgs.msg import Float32
 from sensor_msgs.msg import Joy
-import time
+from geometry_msgs.msg import Twist
 
 NODE = "joy_translator"
 TOPIC = "joy"
+PUBLISH_TOPIC_WHEELS = rospy.get_param("/joy_translator/movement_topic")
+PUBLISH_TOPIC_ARMS = rospy.get_param("/joy_translator/arms_topic")
+PUBLISH_TOPIC_DRUMS = rospy.get_param("/joy_translator/drums_topic")
+MAX_WHEEL_SPEED = rospy.get_param("/joy_translator/max_wheel_speed")
+MAX_ARM_SPEED = rospy.get_param("/joy_translator/max_arm_speed")
+MAX_DRUM_SPEED = rospy.get_param("/joy_translator/max_drum_speed")
 
-# Global so topic initiated at start of node
-publisher = rospy.Publisher('requests', Int32, queue_size = 10)
-tank_turn = False
-toggle_time = 0
+# Publishers
+# Wheel twist
+pub_wheels = rospy.Publisher(PUBLISH_TOPIC_WHEELS,
+                             Twist, 
+                             queue_size=10)
+# Arm Front
+pub_front_arm = rospy.Publisher("front_"+PUBLISH_TOPIC_ARMS, 
+	                            Float32, 
+	                            queue_size=10)
+# Arm Back
+pub_back_arm = rospy.Publisher("back_"+PUBLISH_TOPIC_ARMS, 
+	                           Float32, 
+	                           queue_size=10)
+# Drum Front
+pub_front_drum = rospy.Publisher("front_"+PUBLISH_TOPIC_DRUMS, 
+	                             Float32, 
+	                             queue_size=10)
+# Drum Back
+pub_back_drum = rospy.Publisher("back_"+PUBLISH_TOPIC_DRUMS,
+                                Float32, 
+                                queue_size=10)
 
 def callback(data):
-
-    global publisher
-    global tank_turn
-    global toggle_time
-    time_now = time.time()
+    """Parse Joy data, publish Twist data"""
 
     # Raw controller input data indexes
-
     # data.buttons[index]
     # 0 A : Back Drum Dump
     # 1 B : Front Drum Dump
@@ -32,7 +50,7 @@ def callback(data):
     # 3 Y : Front Drum Dig
     # 4 LB : Back Arm Up
     # 5 RB : Front Arm Up
-    # 6 back : NA
+    # 6 back : Tank Turn
     # 7 start : Supervisor Mode
     # 8 power : NA
     # 9 Button stick left : NA
@@ -48,101 +66,57 @@ def callback(data):
     # 6 cross key left/right : Function 2/3
     # 7 cross key up/down : Function 1/4
 
-    data_out = 0b000000000000
-    # The toggle can only be set once every 0.3 seconds. With out this the toggle will get spammed by Joy. 
-    if data.buttons[6] and (time_now - toggle_time) > 0.3:
-        tank_turn = not tank_turn
-        toggle_time = time.time()
+    twist = Twist()
+    twist.linear.x = data.axes[1] * float(MAX_WHEEL_SPEED)
+    twist.linear.z = data.axes[0] * float(MAX_WHEEL_SPEED)
+    trigger_threshold = 0.0
 
-    data_out <<= 4
-
-    # Wheels (Tank Turn)
-
-    # LWF LWB RWF RWB
-    #  0   0   0   0
-    if tank_turn:
-        if data.axes[1] > 0.5:
-            data_out |= 0b1000
-        
-        elif data.axes[1] < -0.5:
-            data_out |= 0b0100
-
-        if data.axes[4] > 0.5:
-            data_out |= 0b0010
-        
-        elif data.axes[4] < -0.5:
-            data_out |= 0b0001
-
-    # Wheels (Car Turn)
-
-    # F B L R
-    # 0 0 0 0
+    # Use "-(1-data.axes[5])/2" not -1 for variable speed
+    # Front Arm
+    if data.buttons[5] > (1 - data.axes[5]) / 2:
+    	command_front_arm = 1 * MAX_ARM_SPEED
+    elif data.buttons[5] < (1 - data.axes[5]) / 2 and trigger_threshold > data.axes[5]:
+    	command_front_arm = -1 * MAX_ARM_SPEED
     else:
+    	command_front_arm = 0
 
-        if data.axes[1] > 0.5:
-            data_out |= 0b1010
-            
-        elif data.axes[1] < -0.5:
-            data_out |= 0b0101
+    # Back Arm
+    if data.buttons[4] > (1 - data.axes[2]) / 2:
+    	command_back_arm = 1 * MAX_ARM_SPEED
+    elif data.buttons[4] < (1 - data.axes[2]) / 2 and trigger_threshold > data.axes[2]:
+    	command_back_arm = -1 * MAX_ARM_SPEED
+    else:
+    	command_back_arm = 0
 
-        elif data.axes[0] > 0.5:
-            data_out |= 0b0110
-            
-        elif data.axes[0] < -0.5:
-            data_out |= 0b1001
+    # Front Drum
+    if data.buttons[3] > data.buttons[1]:
+        command_front_drum = 1 * MAX_DRUM_SPEED
+    elif data.buttons[3] < data.buttons[1]:
+    	command_front_drum = -1 * MAX_DRUM_SPEED
+    else:
+    	command_front_drum = 0
 
-    # Arms
+    # Back Drum
+    if data.buttons[2] > data.buttons[0]:
+        command_back_drum = 1 * MAX_DRUM_SPEED
+    elif data.buttons[2] < data.buttons[0]:
+    	command_back_drum = -1 * MAX_DRUM_SPEED
+    else:
+    	command_back_drum = 0
 
-    # FAU FAD BAU BAD
-    #  0   0   0   0
-
-    data_out <<= 4
-
-    if data.buttons[5] > 0.5:
-        data_out |= 0b1000
-
-    if data.axes[5] < -0.5:
-        data_out |= 0b0100
-
-    if data.buttons[4] > 0.5:
-        data_out |= 0b0010
-
-    if data.axes[2] < -0.5:
-        data_out |= 0b0001
-
-
-    # Drums
-
-    # FDG FDP BDG BDP
-    #  0   0   0   0
-
-    data_out <<= 4
-
-    if data.buttons[3] > 0.5:
-        data_out |= 0b1000
-
-    if data.buttons[1] > 0.5:
-        data_out |= 0b0100
-
-    if data.buttons[2] > 0.5:
-        data_out |= 0b0010
-
-    if data.buttons[0] > 0.5:
-        data_out |= 0b0001
-
-    publisher.publish(data_out)
-    # rospy.loginfo("Controller: {0:012b}".format(data_out))
-
+    pub_wheels.publish(twist)
+    pub_front_arm.publish(command_front_arm)
+    pub_back_arm.publish(command_back_arm)
+    pub_front_drum.publish(command_front_drum)
+    pub_back_drum.publish(command_back_drum)
     
+
 def start_node():
     try:
-        global publisher
+        print "Controller node started"
         rospy.init_node(NODE, anonymous = True)
-        rate = rospy.Rate(600) # number of hz
-        
-        # Topic subscriber for reading raw controller input 
+        rate = rospy.Rate(600)
         rospy.Subscriber(TOPIC, Joy, callback)
-
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
