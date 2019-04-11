62d8c466be5ddebb5bf52db9d96683911e96a004
Tiger Sachse <tiger@sachse.io>
2019-04-11 12:49:40 -0400

    #217 generalized topic switch

--- packages/communication/ezrassor_topic_switch/source/ezrassor_topic_switch/topic_switch.py
+++ packages/communication/ezrassor_topic_switch/source/ezrassor_topic_switch/topic_switch.py
@@ -1,55 +1,52 @@
-"""Control the flow of information through the EZ-RASSOR's ROS graph.
+"""Control the flow of information through a ROS graph.
 
 Written by Tiger Sachse.
 """
 import rospy
+import threading
 import std_msgs.msg
-import geometry_msgs.msg
 
 
 class OverrideStatus:
     """Automatically maintain an override status."""
     def __init__(self, override_topic):
-        """Initialize this override status with a subscriber."""
-        self.status = False
+        """Initialize this override status with a subscriber.
+        
+        This class possibly doesn't need the lock, but I left it just in case.
+        """
+        self.__status = False
         rospy.Subscriber(
             override_topic,
             std_msgs.msg.Bool,
-            callback=self.update,
+            callback=self.__update,
         )
+        self.__status_lock = threading.Lock()
     
     def __eq__(self, other_status):
         """Allow easy comparisons of this override status with a boolean."""
-        return self.status == other_status
+        self.__status_lock.acquire()
+        status_equality = self.__status == other_status
+        self.__status_lock.release()
 
-    def update(self, data):
+        return status_equality
+
+    def __update(self, status):
         """Update the override status with data from the subscriber."""
-        self.status = data.data
+        self.__status_lock.acquire()
+        self.__status = status.data
+        self.__status_lock.release()
 
 
-def route_item(data, additional_arguments):
+def route_item(item, additional_arguments):
     """Route an item to the output topic if the override status permits it."""
     output_publisher, override_status, expected_status = additional_arguments
 
     if override_status == expected_status:
-        output_publisher.publish(data.data)
-
-
-def get_topic_class(topic_type):
-    """Get a topic class from a given topic type.
-    
-    This is a horrible way to do this. :) Please generalize if you have the time.
-    """
-    if topic_type == "Float32":
-        return std_msgs.msg.Float32
-    elif topic_type == "Twist":
-        return geometry_msgs.msg.Twist
-    else:
-        raise TypeError("Unsupported topic type '%s'" % topic_type)
+        output_publisher.publish(item)
 
 
 def start_node(default_node_name, override_topic, queue_size):
-    """Fire up this node!"""
+    """Fire up the switch."""
     try:
         rospy.init_node(default_node_name)
 
@@ -57,19 +54,35 @@ def start_node(default_node_name, override_topic, queue_size):
         primary_topic = rospy.get_param(rospy.get_name() + "/primary_topic")
         secondary_topic = rospy.get_param(rospy.get_name() + "/secondary_topic")
         output_topic = rospy.get_param(rospy.get_name() + "/output_topic")
-        topic_type = rospy.get_param(rospy.get_name() + "/topic_type")
+        topic_type_module = rospy.get_param(rospy.get_name() + "/topic_type_module")
+        topic_type_class = rospy.get_param(rospy.get_name() + "/topic_type_class")
         override_status = OverrideStatus(override_topic)
-        topic_class = get_topic_class(topic_type)
+
+        # Overwrite the topic type module and topic type class with an imported
+        # module and class. If these can't be imported, an exception is thrown.
+        try:
+            topic_type_module = __import__(
+                topic_type_module,
+                globals(),
+                locals(),
+                [topic_type_class],
+                -1,
+            )
+            topic_type_class = vars(topic_type_module)[topic_type_class]
+        except ImportError:
+            raise ImportError("No topic type module named \"%s\"" % topic_type_module)
+        except KeyError:
+            raise ImportError("No topic type class named \"%s\"" % topic_type_class)
 
         # Create all publishers and subscribers.
         output_publisher = rospy.Publisher(
             output_topic,
-            topic_class,
+            topic_type_class,
             queue_size=queue_size,
         )
         rospy.Subscriber(
             primary_topic,
-            topic_class,
+            topic_type_class,
             callback=route_item,
             callback_args=(
                 output_publisher,
@@ -79,7 +92,7 @@ def start_node(default_node_name, override_topic, queue_size):
         )
         rospy.Subscriber(
             secondary_topic,
-            topic_class,
+            topic_type_class,
             callback=route_item,
             callback_args=(
                 output_publisher,
