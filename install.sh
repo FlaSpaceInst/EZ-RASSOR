91ae873ddae57f32b4d803d426861f893678da79
Tiger Sachse <tgsachse@gmail.com>
2019-06-05 03:20:58 -0400

    comments and final tweaks for the command line arguments portion of the script

--- install.sh
+++ install.sh
@@ -102,33 +102,60 @@ link_and_install() {
     #source_setup bash zsh
 }
 
+# Main entry point of the script.
+#sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > \
+#           /etc/apt/sources.list.d/ros-latest.list'
+#sudo apt-key adv \
+#             --keyserver hkp://ha.pool.sks-keyservers.net:80 \
+#             --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+#sudo apt update
+
+#if [ "$#" = "0" ]; then
+#    link_and_install "communication" "hardware" "autonomy"
+#else
+#    link_and_install "$@"
+#fi
+
+
+
+
+
+# Print an error message and exit this script.
+throw_error() {
+    printf "%s\n" "$@"
+    exit 1
+}
+
+# The main entry point to the installation script.
+# Determine the user's OS version and an appropriate ROS version.
 OS_VERSION="$(lsb_release -sc)"
-ROS_VERSION=false
 if [ "$OS_VERSION" = "xenial" ]; then
     ROS_VERSION="kinetic"
 elif [ "$OS_VERSION" = "bionic" ]; then
     ROS_VERSION="melodic"
 else
-    printf "\n"
-           "This script is only tested on Ubuntu Xenial and Ubuntu Bionic with ROS"
-           "Kinetic and ROS Melodic. Your operating system is not supported. :("
-           "You may still attempt to install the packages that you want manually"
-           "using Catkin. Refer to the ROS wiki for instructions."
-    exit 1
+    throw_error "This script is only tested on Ubuntu Xenial and Ubuntu Bionic with ROS" \
+                "Kinetic and ROS Melodic. Your operating system is not supported. :(" \
+                "You may still attempt to install the packages that you want manually" \
+                "using Catkin. Refer to the ROS wiki for instructions."
 fi
 
-SET_COMPONENTS=false
-MISSING_COMPONENTS_LIST=false
-
+# Set a few installation flags with defaults.
 INSTALL_AUTONOMY=true
 INSTALL_EXTERNALS=true
 INSTALL_DASHBOARD=true
 INSTALL_SIMULATION=true
 INSTALL_COMMUNICATION=true
-
-SET_INSTALLATION_METHOD=false
 INSTALLATION_METHOD="automatic"
+
+# Determine the user's command line arguments.
+SET_COMPONENTS=false
+SET_INSTALLATION_METHOD=false
+MISSING_COMPONENTS_LIST=false
 for ARGUMENT in "$@"; do
+
+    # If the user has included the "--method" flag, set the INSTALLATION_METHOD
+    # appropriately.
     if [ "$SET_INSTALLATION_METHOD" = "true" ]; then
         if [ "$ARGUMENT" = "automatic" ]; then
             INSTALLATION_METHOD="automatic"
@@ -137,11 +164,13 @@ for ARGUMENT in "$@"; do
         elif [ "$ARGUMENT" = "packages-only" ]; then
             INSTALLATION_METHOD="packages-only"
         else
-            printf "Invalid installation method: %s.\n" "$ARGUMENT"
-            exit 1
+            throw_error "Invalid installation method: $ARGUMENT"
         fi
         SET_INSTALLATION_METHOD=false
         continue
+
+    # If the user has included the "--components" flag, set the
+    # INSTALL_COMPONENT flags appropriately.
     elif [ "$SET_COMPONENTS" = "true" ]; then
         if [ "$ARGUMENT" = "autonomy" ]; then
             MISSING_COMPONENTS_LIST=false
@@ -168,6 +197,7 @@ for ARGUMENT in "$@"; do
         fi
     fi
 
+    # Check the user's arguments for supported flags.
     case "$ARGUMENT" in
         --method)
             SET_INSTALLATION_METHOD=true
@@ -184,13 +214,11 @@ for ARGUMENT in "$@"; do
     esac
 done
 
+# Throw an error if the user used a flag incorrectly.
 if [ "$SET_INSTALLATION_METHOD" = "true" ]; then
-    printf "Missing installation method.\n"
-    exit 1
-# elif no components selected
+    throw_error "Missing installation method."
 elif [ "$MISSING_COMPONENTS_LIST" = "true" ]; then
-    printf "Missing components list.\n"
-    exit 1
+    throw_error "Missing components list."
 fi
 
 echo "$INSTALL_AUTONOMY"
@@ -199,18 +227,5 @@ echo "$INSTALL_DASHBOARD"
 echo "$INSTALL_SIMULATION"
 echo "$INSTALL_COMMUNICATION"
 echo "$INSTALLATION_METHOD"
+echo "$OS_VERSION"
 echo "$ROS_VERSION"
-
-# Main entry point of the script.
-#sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > \
-#           /etc/apt/sources.list.d/ros-latest.list'
-#sudo apt-key adv \
-#             --keyserver hkp://ha.pool.sks-keyservers.net:80 \
-#             --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
-#sudo apt update
-
-#if [ "$#" = "0" ]; then
-#    link_and_install "communication" "hardware" "autonomy"
-#else
-#    link_and_install "$@"
-#fi
