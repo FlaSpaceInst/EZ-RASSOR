3c8e15ebf0b6b76694b42e1f392df52d172f0ba0
Tiger Sachse <tgsachse@gmail.com>
2019-06-17 18:06:03 -0400

    reorganize script a bit, finish parsing of cmdline args

--- install.sh
+++ install.sh
@@ -2,6 +2,23 @@
 #
 #
 
+# A collection of necessary constants for this script.
+USER_SHELLS="bash zsh"
+SH_SETUP_FILE="setup.sh"
+ROSINSTALL_FILE="ros-comm.rosinstall"
+KEY_SERVER="hkp://ha.pool.sks-keyservers.net:80"
+RECV_KEY="C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654"
+CATKIN_MAKE_ISOLATED_BIN="$WORKSPACE_SOURCE_RELATIVE_DIR/catkin/bin/catkin_make_isolated"
+EXTERNALS_DIR="external"
+SUPERPACKAGES_DIR="packages"
+MOCK_INSTALL_RELATIVE_DIR="install"
+WORKSPACE_SOURCE_RELATIVE_DIR="src"
+MANUAL_ROS_INSTALL_DIR="$HOME/.ezrassor/ros"
+AUTOMATIC_ROS_PARTIAL_INSTALL_DIR="/opt/ros"
+WORKSPACE_PARTIAL_DIR="/tmp/ezrassor_workspace"
+MANUAL_EZRASSOR_INSTALL_DIR="$HOME/.ezrassor/core"
+
+# Throw a help message at the user.
 throw_help() {
     printf "Usage: sh install.sh <mode> [--flags]"
 }
@@ -41,21 +58,6 @@ add_ros_repository() {
     sudo apt update
 }
 
-# Install buildtools for ROS.
-install_buildtools() {
-    require "sudo" "apt"
-    sudo apt install -y python-pip \
-                        python-rosdep \
-                        python-rosinstall-generator \
-                        python-wstool \
-                        python-rosinstall \
-                        build-essential
-    set +e
-    sudo rosdep init
-    set -e
-    rosdep update
-}
-
 # Source setup files within a given directory in the user's RC files.
 source_setups_in_directory() {
     must_restart=false
@@ -88,20 +90,6 @@ source_setups_in_directory() {
     fi
 }
 
-# Install ROS automatically with APT.
-install_ros_automatically() {
-    ros_version="$1"
-    automatic_ros_install_dir="$2"
-    require "sudo" "apt"
-    add_ros_repository
-    sudo apt install -y "ros-${ros_version}-ros-base"
-    set +e
-    sudo rosdep init
-    set -e
-    rosdep update
-    source_setups_in_directory "$automatic_ros_install_dir" 
-}
-
 # Install ROS manually.
 install_ros_manually() {
     ros_version="$1"
@@ -132,6 +120,35 @@ install_ros_manually() {
     source_setups_in_directory "$MANUAL_ROS_INSTALL_DIR"
 }
 
+# Install ROS automatically with APT.
+install_ros_automatically() {
+    os_version="$1"
+    ros_version="$2"
+    require "sudo" "apt"
+    add_ros_repository "$os_version"
+    sudo apt install -y "ros-${ros_version}-ros-base"
+    set +e
+    sudo rosdep init
+    set -e
+    rosdep update
+    source_setups_in_directory "$AUTOMATIC_ROS_PARTIAL_INSTALL_DIR/$ros_version"
+}
+
+# Install buildtools for ROS.
+install_buildtools() {
+    require "sudo" "apt"
+    sudo apt install -y python-pip \
+                        python-rosdep \
+                        python-rosinstall-generator \
+                        python-wstool \
+                        python-rosinstall \
+                        build-essential
+    set +e
+    sudo rosdep init
+    set -e
+    rosdep update
+}
+
 # Install only EZ-RASSOR packages.
 install_ezrassor_packages() {
     require "pip" "rosdep" "catkin_make"
@@ -149,10 +166,12 @@ install_ezrassor_packages() {
                     shift
                     ;;
             esac
+        fi
         for ARGUMENT in "$@"; do
 
 
         done
+        
     # Create a temporary workspace.
     workspace_dir="${WORKSPACE_PARTIAL_DIR}_$(date +%s)"
     workspace_source_dir="$workspace_dir/$WORKSPACE_SOURCE_RELATIVE_DIR"
@@ -209,21 +228,7 @@ install_ezrassor_packages() {
     source_setups_in_directory "$MANUAL_EZRASSOR_INSTALL_DIR" 
 }
 
-# The main entry point to the installation script.
-USER_SHELLS="bash zsh"
-SH_SETUP_FILE="setup.sh"
-ROSINSTALL_FILE="ros-comm.rosinstall"
-KEY_SERVER="hkp://ha.pool.sks-keyservers.net:80"
-RECV_KEY="C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654"
-CATKIN_MAKE_ISOLATED_BIN="$WORKSPACE_SOURCE_RELATIVE_DIR/catkin/bin/catkin_make_isolated"
-EXTERNALS_DIR="external"
-SUPERPACKAGES_DIR="packages"
-MOCK_INSTALL_RELATIVE_DIR="install"
-WORKSPACE_SOURCE_RELATIVE_DIR="src"
-MANUAL_ROS_INSTALL_DIR="$HOME/.ezrassor/ros"
-WORKSPACE_PARTIAL_DIR="/tmp/ezrassor_workspace"
-MANUAL_EZRASSOR_INSTALL_DIR="$HOME/.ezrassor/core"
-
+# The main entry point of this script.
 # Throw a message if the script quits early, and tell the script to quit after
 # any non-zero error message.
 trap 'throw_error "Something went horribly wrong!"' 0
@@ -231,26 +236,38 @@ set -e
 
 case "$1" in
     "ros")
-        if [ "$2" = "--from-source=kinetic" ]; then
-            install_ros_manually "kinetic"
-        elif [ "$2" = "--from-source=melodic" ]; then
-            install_ros_manually "melodic"
+        case "$2" in
+            "--from-source="*)
+                case $2 in
+                    *"kinetic")
+                        install_ros_manually "kinetic"
+                        exit 0
+                        ;;
+                    *"melodic")
+                        install_ros_manually "melodic"
+                        exit 0
+                        ;;
+                    *)
+                        throw_error "That distribution of ROS is not supported by" \
+                                    "this script. Try either \"--from-source=kinetic\"" \
+                                    "or \"--from-source=melodic\"."
+                        ;;
+                esac
+                ;;
+        esac
+        require "lsb_release"
+        os_version="$(lsb_release -sc)"
+        if [ "$os_version" = "xenial" ]; then
+            ros_version="kinetic"
+        elif [ "$os_version" = "bionic" ]; then
+            ros_version="melodic"
         else
-            require "lsb_release"
-            os_version="$(lsb_release -sc)"
-            if [ "$os_version" = "xenial" ]; then
-                ros_version="kinetic"
-            elif [ "$os_version" = "bionic" ]; then
-                ros_version="melodic"
-            else
-                throw_error "This script can only automatically install ROS for"
-                            "Ubuntu Xenial and Ubuntu Bionic. Your operating system" \
-                            "is not supported. :( You can still try to install ROS" \
-                            "manually using the \"--from-source=\" parameter."
-            fi
-            automatic_ros_install_dir="/opt/ros/$ros_version"
-            install_ros_automatically "$ros_version" "$automatic_ros_install_dir"
+            throw_error "This script can only automatically install ROS for"
+                        "Ubuntu Xenial and Ubuntu Bionic. Your operating system" \
+                        "is not supported. :( You can still try to install ROS" \
+                        "manually using the \"--from-source=\" parameter."
         fi
+        install_ros_automatically "$os_version" "$ros_version"
         ;;
     "buildtools")
         install_buildtools
