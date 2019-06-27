e3773dad217aa1a411c076d030e277c48c5748d3
Tiger Sachse <tgsachse@gmail.com>
2019-06-27 21:07:00 +0000

    update setup.pys with better comments and style, closes #242

--- packages/autonomy/ezrassor_autonomous_control/setup.py
+++ packages/autonomy/ezrassor_autonomous_control/setup.py
@@ -1,12 +1,12 @@
+"""Setup the ezrassor_autonomous_control ROS package.
+
+Written by Tiger Sachse.
+"""
 import distutils.core
 import catkin_pkg.python_setup
 
 setup_arguments = catkin_pkg.python_setup.generate_distutils_setup(
-    packages=(
-        "ezrassor_autonomous_control",
-    ),
-    package_dir={
-       "" : "source",
-    },
+    packages=("ezrassor_autonomous_control", ),
+    package_dir={"" : "source"},
 )
 distutils.core.setup(**setup_arguments)
