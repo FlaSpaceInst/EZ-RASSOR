f7063287c6037d70270315284fa65bfee36f439c
Tiger Sachse <tgsachse@gmail.com>
2019-06-28 12:12:38 -0700

    small tweaks to move meshes script

--- blender/move_meshes.sh
+++ blender/move_meshes.sh
@@ -3,7 +3,4 @@
 # Written by Ronald Marrero.
 MESH_DIR="../packages/simulation/ezrassor_sim_description/meshes"
 
-mv EZRASSOR_BASE_UNIT.dae "$MESHES_DIR"/base_unit.dae
-mv EZRASSOR_DRUM_ARM.dae "$MESHES_DIR"/drum_arm.dae
-mv EZRASSOR_DRUM.dae "$MESHES_DIR"/drum.dae
-mv EZRASSOR_WHEEL.dae "$MESHES_DIR"/wheel.dae
+mv *.dae "$MESHES_DIR"
