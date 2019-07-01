c11bb0133918c707d934f63f32dc775abc04dca8
Tiger Sachse <tgsachse@gmail.com>
2019-07-01 23:29:25 +0000

    fix small issues plus add comments

--- packages/extras/ezrassor_dashboard/source/ezrassor_status_monitor/status_monitor.py
+++ packages/extras/ezrassor_dashboard/source/ezrassor_status_monitor/status_monitor.py
@@ -11,44 +11,40 @@ import rospy
 import numpy
 import subprocess
 import std_msgs.msg
-import multiprocessing
 
 
 def get_pids(process_names):
-    """"""
+    """Get a list of PIDs from a list of process names."""
     pids = []
     for process_name in process_names:
-        returned_pids = subprocess.check_output(
-            "pgrep -f {0}".format(process_name),
-            shell=True,
-        )
-
-        if returned_pids is not None:
-            pids.extend((int(pid) for pid in returned_pids.split("\n"))) #
+        try:
+            for pid in subprocess.check_output(("pgrep", "-f", process_name)).split():
+                pids.append(pid)
+        except subprocess.CalledProcessError:
+            pass
 
     return pids
 
 
 def get_cpu_usage(pids):
-    """"""
+    """Get the CPU usage of a list of PIDs."""
     cpu_usage = 0.0
     for pid in pids:
         unparsed_cpu_usage = subprocess.check_output(
-            "top -b -n 1 -p {0}".format(pid),
-            shell=True,
+            ("top", "-b", "-n", "1", "-p", pid),
         )
 
-        cpu_usage_line_items = unparsed_cpu_usage[-1].split()
+        cpu_usage_line_items = unparsed_cpu_usage.split()
         try:
-            cpu_usage += float(cpu_usage_line_items[8])
-        except ValueError:
+            cpu_usage += float(cpu_usage_line_items[-4])
+        except (ValueError, IndexError):
             pass
 
     return cpu_usage
 
 
 def start_node(default_node_name, process_names, cpu_usage_topic, queue_size):
-    """"""
+    """Start the fun!"""
     try:
         rospy.init_node(default_node_name, anonymous=True)
         cpu_usage_publisher = rospy.Publisher(
@@ -58,7 +54,7 @@ def start_node(default_node_name, process_names, cpu_usage_topic, queue_size):
         )
 
         while not rospy.is_shutdown():
-            process_pids = get_pids(process_names)
-            cpu_usage_publisher.publish(get_cpu_usage(process_pids))
+            pids = get_pids(process_names)
+            cpu_usage_publisher.publish(get_cpu_usage(pids))
     except rospy.ROSInterruptException:
         pass
