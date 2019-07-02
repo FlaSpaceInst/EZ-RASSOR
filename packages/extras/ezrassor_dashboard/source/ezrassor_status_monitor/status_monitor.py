c8d012e6b4d037babb6231fee142298e9f93dbd8
Tiger Sachse <tgsachse@gmail.com>
2019-07-01 22:34:32 -0700

    add memory and battery usage, add psutil dep, fixes #273

--- packages/extras/ezrassor_dashboard/source/ezrassor_status_monitor/status_monitor.py
+++ packages/extras/ezrassor_dashboard/source/ezrassor_status_monitor/status_monitor.py
@@ -1,60 +1,57 @@
-"""Track the CPU usage of all EZ-RASSOR processes.
-
-This node presents the total CPU usage in a way that can be understood by the
-ezrassor_dashboard. It separates the CPU consumption of Gazebo from the processes
-being run in ROS, which allows researchers to better understand the power drain
-and CPU demands of any autonomous systems they are testing.
+"""Track the CPU and memory usage of all EZ-RASSOR processes.
 
 Written by Samuel Lewis and Tiger Sachse.
 """
 import rospy
-import numpy
-import subprocess
+import psutil
 import std_msgs.msg
 
 
-def get_pids(process_names):
-    """Get a list of PIDs from a list of process names."""
-    pids = []
-    for process_name in process_names:
-        try:
-            for pid in subprocess.check_output(("pgrep", "-f", process_name)).split():
-                pids.append(pid)
-        except subprocess.CalledProcessError:
-            pass
-
-    return pids
-
-
-def get_cpu_usage(pids):
-    """Get the CPU usage of a list of PIDs."""
-    cpu_usage = 0.0
-    for pid in pids:
-        unparsed_cpu_usage = subprocess.check_output(
-            ("top", "-b", "-n", "1", "-p", pid),
-        )
-
-        cpu_usage_line_items = unparsed_cpu_usage.split()
-        try:
-            cpu_usage += float(cpu_usage_line_items[-4])
-        except (ValueError, IndexError):
-            pass
-
-    return cpu_usage
-
-
-def start_node(default_node_name, process_names, cpu_usage_topic, queue_size):
+def start_node(
+    default_node_name,
+    process_names,
+    cpu_usage_topic,
+    memory_usage_topic,
+    battery_usage_topic,
+    queue_size):
     """Start the fun!"""
     try:
-        rospy.init_node(default_node_name, anonymous=True)
+        rospy.init_node(default_node_name)
         cpu_usage_publisher = rospy.Publisher(
             cpu_usage_topic,
             std_msgs.msg.Float64,
             queue_size=queue_size,
         )
+        memory_usage_publisher = rospy.Publisher(
+            memory_usage_topic,
+            std_msgs.msg.Float64,
+            queue_size=queue_size,
+        )
+        battery_usage_publisher = rospy.Publisher(
+            battery_usage_topic,
+            std_msgs.msg.Float64,
+            queue_size=queue_size,
+        )
 
+        # For every iteration of this loop: add up all of the CPU percentages and
+        # memory percentages for every process that matches one of the target
+        # process names, then publish the totals.
+        cpu_count = psutil.cpu_count()
         while not rospy.is_shutdown():
-            pids = get_pids(process_names)
-            cpu_usage_publisher.publish(get_cpu_usage(pids))
+            cpu_usage = 0.0
+            memory_usage = 0.0
+            for process in psutil.process_iter():
+                with process.oneshot():
+                    if process.name() in process_names:
+                        cpu_usage += process.cpu_percent()
+                        memory_usage += process.memory_percent()
+            cpu_usage_publisher.publish(cpu_usage / cpu_count)
+            memory_usage_publisher.publish(memory_usage)
+
+            # Publish battery information as well.
+            battery_usage = psutil.sensors_battery()
+            if battery_usage is not None:
+                battery_usage_publisher.publish(battery_usage.percent)
+
     except rospy.ROSInterruptException:
         pass
