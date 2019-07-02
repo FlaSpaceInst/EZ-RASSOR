ad37af728e99e669d6d0556533fd124f0b2e73c4
Tiger Sachse <tiger@sachse.io>
2019-07-02 10:47:02 -0700

    flesh out gui, add front page and change welcome menu to help menu, finishes #274

--- packages/extras/ezrassor_dashboard/source/ezrassor_dashboard/ezrassor_dashboard.cpp
+++ packages/extras/ezrassor_dashboard/source/ezrassor_dashboard/ezrassor_dashboard.cpp
@@ -1,14 +1,14 @@
 #include <QApplication>
 #include "main_window.h"
-#include "welcome_window.h"
+#include "help_window.h"
 
 int main(int argument_count, char** argument_vector) {
     QApplication application(argument_count, argument_vector);
 
     MainWindow main_window;
     main_window.show();
-    WelcomeWindow welcome_window;
-    welcome_window.show();
+    HelpWindow help_window;
+    help_window.show();
 
     return application.exec();
 }
