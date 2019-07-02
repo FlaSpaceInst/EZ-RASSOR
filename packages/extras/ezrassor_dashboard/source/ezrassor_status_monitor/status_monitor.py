c6ec47015612d17ba903278e0ae9dbf035ca4f28
Tiger Sachse <tiger@sachse.io>
2019-07-02 06:00:58 +0000

    finally status is acceptable, finished working on monitor

--- packages/extras/ezrassor_dashboard/source/ezrassor_status_monitor/status_monitor.py
+++ packages/extras/ezrassor_dashboard/source/ezrassor_status_monitor/status_monitor.py
@@ -9,7 +9,7 @@ import std_msgs.msg
 
 def start_node(
     default_node_name,
-    process_names,
+    trigger_string,
     cpu_usage_topic,
     memory_usage_topic,
     battery_usage_topic,
@@ -33,18 +33,24 @@ def start_node(
             queue_size=queue_size,
         )
 
-        # For every iteration of this loop: add up all of the CPU percentages and
-        # memory percentages for every process that matches one of the target
-        # process names, then publish the totals.
+        # Publish the sum of CPU and memory usage for all processes whose calls
+        # contain the trigger string.
+        sleep_rate = rospy.Rate(1)
         cpu_count = psutil.cpu_count()
         while not rospy.is_shutdown():
             cpu_usage = 0.0
             memory_usage = 0.0
             for process in psutil.process_iter():
-                with process.oneshot():
-                    if process.name() in process_names:
-                        cpu_usage += process.cpu_percent()
-                        memory_usage += process.memory_percent()
+                try:
+                    with process.oneshot():
+                        for chunk in process.cmdline():
+                            if trigger_string in chunk:
+                                cpu_usage += process.cpu_percent()
+                                memory_usage += process.memory_percent()
+                                break
+                except psutil.NoSuchProcess:
+                    pass
+
             cpu_usage_publisher.publish(cpu_usage / cpu_count)
             memory_usage_publisher.publish(memory_usage)
 
@@ -52,6 +58,9 @@ def start_node(
             battery_usage = psutil.sensors_battery()
             if battery_usage is not None:
                 battery_usage_publisher.publish(battery_usage.percent)
+
+            # Don't do this too fast, lest you waste precious CPU cycles.
+            sleep_rate.sleep()
 
     except rospy.ROSInterruptException:
         pass
