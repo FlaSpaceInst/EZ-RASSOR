2b3386ca9c1d35785e5a6cda5366ea2d28a6ba96
Tiger Sachse <tgsachse@gmail.com>
2019-07-02 10:47:02 -0700

    flesh out gui, add front page and change welcome menu to help menu, finishes #274

--- packages/extras/ezrassor_dashboard/source/ezrassor_dashboard/welcome_window.cpp
+++
@@ -1,9 +0,0 @@
-#include "welcome_window.h"
-
-WelcomeWindow::WelcomeWindow(QWidget *parent) : QWidget(parent) {
-    setupUi(this);
-}
-
-void WelcomeWindow::on_ConnectButton_pressed(void) {
-    printf("hello world\n");
-}
