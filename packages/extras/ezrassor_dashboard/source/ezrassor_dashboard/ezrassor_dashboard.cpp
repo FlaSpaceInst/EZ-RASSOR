aa0851168cde32b381b7d0a9b6dc8eab4ff7cde0
Tiger Sachse <tgsachse@gmail.com>
2019-07-03 13:56:11 -0400

    this mess sort of launches the ros node? still working on it

--- packages/extras/ezrassor_dashboard/source/ezrassor_dashboard/ezrassor_dashboard.cpp
+++ packages/extras/ezrassor_dashboard/source/ezrassor_dashboard/ezrassor_dashboard.cpp
@@ -1,6 +1,7 @@
 #include <QApplication>
 #include "main_window.h"
 #include "about_window.h"
+#include "topic_translator.h"
 
 int main(int argumentCount, char** argumentVector) {
     QApplication application(argumentCount, argumentVector);
@@ -9,6 +10,30 @@ int main(int argumentCount, char** argumentVector) {
     mainWindow.show();
     AboutWindow aboutWindow;
     aboutWindow.show();
+    TopicTranslator topicTranslator(
+        "dashboard",
+        "cpu_usage",
+        "memory_usage",
+        "battery_usage",
+        100
+    );
+
+    //topicTranslator.connectToMaster("whatthefuck");
+    //topicTranslator.start();
+
+    application.connect(
+        &topicTranslator,
+        SIGNAL(processorDataReceived(int)),
+        mainWindow.CPUUsageBar,
+        SLOT(setValue(int))
+    );
+
+    application.connect(
+        &mainWindow,
+        SIGNAL(connectionRequested(const std::string&)),
+        &topicTranslator,
+        SLOT(connectToMaster(const std::string&))
+    );
 
     return application.exec();
 }
--- packages/extras/ezrassor_dashboard/source/ezrassor_dashboard/topic_translator.cpp
+++ packages/extras/ezrassor_dashboard/source/ezrassor_dashboard/topic_translator.cpp
@@ -1,29 +1,61 @@
-#include <QObject>
+#include <QThread>
 #include "ros/ros.h"
 #include "std_msgs/Float64.h"
 #include "topic_translator.h"
 
-bool TopicTranslator::connectToMasterconst std::string& masterURI) {
+void TopicTranslator::connectToMaster(const std::string& masterURI) {
+    std::cout << masterURI << std::endl;
     std::map<std::string, std::string> masterURIRemap;
     masterURIRemap["__master"] = masterURI;
     ros::init(masterURIRemap, nodeName);
 
     if (!ros::master::check()) {
-        return false;
+        return;// false;
     }
 
+    // explicitly needed bc nodehandle out of skope
+    ros::start();
     ros::NodeHandle nodeHandle;
 
     processorUsageSubscriber = nodeHandle.subscribe(
-        "cpu_usage",
-        1000,
-        &TopicTranslator::handleProcessorDatum,
+        processorUsageTopic,
+        queueSize,
+        &TopicTranslator::handleProcessorData,
+        this
+    );
+    memoryUsageSubscriber = nodeHandle.subscribe(
+        memoryUsageTopic,
+        queueSize,
+        &TopicTranslator::handleMemoryData,
+        this
+    );
+    batteryUsageSubscriber = nodeHandle.subscribe(
+        batteryUsageTopic,
+        queueSize,
+        &TopicTranslator::handleBatteryData,
         this
     );
 
-    return true;
+    //return true;
+    start();
 }
 
-void TopicTranslator::handleProcessorDatum(const std_msgs::Float64::ConstPtr& message) {
-    Q_EMIT processorDatumHandled((double) message->data);
+int TopicTranslator::exec(void) {
+    ros::spin();
+    //std::cout << "ROS has shutdown: closing Dashboard..." << std::endl;
+
+    //Q_EMIT 
+    return 0;
+}
+
+void TopicTranslator::handleProcessorData(const std_msgs::Float64::ConstPtr& message) {
+    Q_EMIT processorDataReceived((int) message->data);
+}
+
+void TopicTranslator::handleMemoryData(const std_msgs::Float64::ConstPtr& message) {
+    Q_EMIT memoryDataReceived((int) message->data);
+}
+
+void TopicTranslator::handleBatteryData(const std_msgs::Float64::ConstPtr& message) {
+    Q_EMIT batteryDataReceived((int) message->data);
 }
