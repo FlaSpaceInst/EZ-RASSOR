1c13f039996d63fa62ebb2bbcad803fc41ba7806
Tiger Sachse <tgsachse@gmail.com>
2019-07-03 11:47:18 -0400

    change URL to URI

--- packages/extras/ezrassor_dashboard/source/ezrassor_dashboard/topic_translator.cpp
+++ packages/extras/ezrassor_dashboard/source/ezrassor_dashboard/topic_translator.cpp
@@ -1,12 +1,12 @@
-#include <QObject> //
+#include <QObject>
 #include "ros/ros.h"
 #include "std_msgs/Float64.h"
 #include "topic_translator.h"
 
-bool TopicTranslator::connectToMaster(const std::string& masterURL) {
-    std::map<std::string, std::string> masterURLRemap;
-    masterURLRemap["__master"] = masterURL;
-    ros::init(masterURLRemap, nodeName);
+bool TopicTranslator::connectToMasterconst std::string& masterURI) {
+    std::map<std::string, std::string> masterURIRemap;
+    masterURIRemap["__master"] = masterURI;
+    ros::init(masterURIRemap, nodeName);
 
     if (!ros::master::check()) {
         return false;
