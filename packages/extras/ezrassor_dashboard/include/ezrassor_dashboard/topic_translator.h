793794da7d6f5587a4997b6aca7721676a00998c
Tiger Sachse <tgsachse@gmail.com>
2019-07-03 13:56:11 -0400

    this mess sort of launches the ros node? still working on it

--- packages/extras/ezrassor_dashboard/include/ezrassor_dashboard/topic_translator.h
+++ packages/extras/ezrassor_dashboard/include/ezrassor_dashboard/topic_translator.h
@@ -1,23 +1,46 @@
 #ifndef TOPIC_TRANSLATOR_HEADER
 #define TOPIC_TRANSLATOR_HEADER
 
-#include <QObject>
+#include <QThread>
 #include <ros/ros.h>
 #include "std_msgs/Float64.h"
 
-class TopicTranslator : public QObject {
+class TopicTranslator : public QThread {
     Q_OBJECT
     public:
-        TopicTranslator(const std::string& nodeName) : nodeName(nodeName) {};
-        bool connectToMaster(const std::string&);
+        TopicTranslator(
+            const std::string& nodeName,
+            const std::string& processorUsageTopic,
+            const std::string& memoryUsageTopic,
+            const std::string& batteryUsageTopic,
+            int queueSize)
+            : nodeName(nodeName),
+              processorUsageTopic(processorUsageTopic),
+              memoryUsageTopic(memoryUsageTopic),
+              batteryUsageTopic(batteryUsageTopic),
+              queueSize(queueSize) {};
+        int exec(void);
+
+    public slots:
+        void connectToMaster(const std::string&);
 
     signals:
-        void processorDatumHandled(double);
+        void processorDataReceived(int);
+        void memoryDataReceived(int);
+        void batteryDataReceived(int);
 
     private:
+        int queueSize;
         const std::string& nodeName;
+        const std::string& processorUsageTopic;
+        const std::string& memoryUsageTopic;
+        const std::string& batteryUsageTopic;
         ros::Subscriber processorUsageSubscriber;
+        ros::Subscriber memoryUsageSubscriber;
+        ros::Subscriber batteryUsageSubscriber;
         
-        void handleProcessorDatum(const std_msgs::Float64::ConstPtr&);
+        void handleProcessorData(const std_msgs::Float64::ConstPtr&);
+        void handleMemoryData(const std_msgs::Float64::ConstPtr&);
+        void handleBatteryData(const std_msgs::Float64::ConstPtr&);
 };
 #endif
--- packages/extras/ezrassor_dashboard/source/ezrassor_dashboard/main_window.cpp
+++ packages/extras/ezrassor_dashboard/source/ezrassor_dashboard/main_window.cpp
@@ -1,5 +1,16 @@
+#include <QWidget>
 #include "main_window.h"
 
 MainWindow::MainWindow(QWidget *parent) : QWidget(parent) {
     setupUi(this);
+    connect(
+        ConnectButton,
+        SIGNAL(clicked()),
+        this,
+        SLOT(handleConnectionRequest())
+    );
+}
+
+void MainWindow::handleConnectionRequest(void) {
+    Q_EMIT connectionRequested(ROSMasterURILineEdit->text().toStdString());
 }
