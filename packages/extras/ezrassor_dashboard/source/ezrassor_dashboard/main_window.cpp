d650d2149a9e72f1d0f21db11e3e8c77d2ffe730
Tiger Sachse <tgsachse@gmail.com>
2019-07-03 14:26:56 -0400

    comment drive

--- packages/extras/ezrassor_dashboard/source/ezrassor_dashboard/main_window.cpp
+++ packages/extras/ezrassor_dashboard/source/ezrassor_dashboard/main_window.cpp
@@ -1,16 +1,20 @@
+// The MainWindow class holds all of the interfaces for the Dashboard.
+// Written by Tiger Sachse.
 #include <QWidget>
 #include "main_window.h"
 
+// Initialize this MainWindow class and set up the UI.
 MainWindow::MainWindow(QWidget *parent) : QWidget(parent) {
     setupUi(this);
     connect(
-        ConnectButton,
+        connectButton,
         SIGNAL(clicked()),
         this,
         SLOT(handleConnectionRequest())
     );
 }
 
+// When a connection is requested, emit a signal with the appropriate Master URI.
 void MainWindow::handleConnectionRequest(void) {
-    Q_EMIT connectionRequested(ROSMasterURILineEdit->text().toStdString());
+    Q_EMIT connectionRequested(masterURILineEdit->text().toStdString());
 }
