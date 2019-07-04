e9ebc25b46ce605ba5f6f57af3df0d2ad38b69fd
Tiger Sachse <tgsachse@gmail.com>
2019-07-04 17:15:16 -0400

    comment drive and reorder methods

--- packages/extras/ezrassor_dashboard/source/ezrassor_dashboard/main_window.cpp
+++ packages/extras/ezrassor_dashboard/source/ezrassor_dashboard/main_window.cpp
@@ -14,7 +14,24 @@ MainWindow::MainWindow(QWidget *parent) : QWidget(parent) {
     );
 }
 
+// Change the GUI to disable new connections.
+void MainWindow::finalizeConnection(void) {
+    connectButton->setText("Disconnect");
+    masterURILineEdit->setEnabled(false);
+}
+
+// Change the GUI to enable reconnections.
+void MainWindow::finalizeDisconnection(void) {
+    connectButton->setText("Connect");
+    masterURILineEdit->setEnabled(true);
+}
+
+// Attempt to connect or disconnect from ROS based on the state of the connectButton.
 void MainWindow::handleConnectButtonPress(void) {
+
+    // I tried making the button checkable, but then it looked grayed out when
+    // checked which I didn't like. Instead, I check the button's text to determine
+    // its state. This isn't the cleanest but none of us are perfect, right?
     if (connectButton->text() == "Connect") {
         Q_EMIT connectionRequested(masterURILineEdit->text().toStdString());
     }
@@ -23,17 +40,6 @@ void MainWindow::handleConnectButtonPress(void) {
     }
 }
 
-#include <iostream>
 void MainWindow::recoverFromConnectionFailure(void) {
     std::cout << "failed" << std::endl;
-}
-
-void MainWindow::finalizeConnection(void) {
-    connectButton->setText("Disconnect");
-    masterURILineEdit->setEnabled(false);
-}
-
-void MainWindow::finalizeDisconnection(void) {
-    connectButton->setText("Connect");
-    masterURILineEdit->setEnabled(true);
 }
