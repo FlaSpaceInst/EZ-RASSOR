eee92e9ab1e7379bab1680659ec670cf6d95c3eb
Tiger Sachse <tgsachse@gmail.com>
2019-07-06 17:22:26 -0400

    post a help message if ros isnt running, also close app if main window closes, fixes #286

--- packages/extras/ezrassor_dashboard/include/ezrassor_dashboard/main_window.h
+++ packages/extras/ezrassor_dashboard/include/ezrassor_dashboard/main_window.h
@@ -3,11 +3,18 @@
 #ifndef MAIN_WINDOW_HEADER
 #define MAIN_WINDOW_HEADER
 #include <QWidget>
+#include <QCloseEvent>
 #include "ui_main_window.h"
 
 class MainWindow : public QWidget, public Ui_mainWindow {
     Q_OBJECT
     public:
         MainWindow(QWidget *parent = NULL);
+
+    signals:
+        void closed(void);
+
+    protected:
+        void closeEvent(QCloseEvent*);
 };
 #endif
--- packages/extras/ezrassor_dashboard/source/ezrassor_dashboard/main_window.cpp
+++ packages/extras/ezrassor_dashboard/source/ezrassor_dashboard/main_window.cpp
@@ -1,9 +1,15 @@
 // The MainWindow class holds all of the interfaces for the Dashboard.
 // Written by Tiger Sachse.
 #include <QWidget>
+#include <QCloseEvent>
 #include "main_window.h"
 
 // Initialize this MainWindow class and set up the UI.
 MainWindow::MainWindow(QWidget *parent) : QWidget(parent) {
     setupUi(this);
+}
+
+void MainWindow::closeEvent(QCloseEvent* closeEvent) {
+    Q_EMIT closed();
+    closeEvent->accept();
 }
