aee64304c8cb196fe73d3a6858a9e88375f5b909
Tiger Sachse <tgsachse@gmail.com>
2019-07-06 17:07:12 -0400

    show help menu if cant immediately connect to ROS master

---
+++ packages/extras/ezrassor_dashboard/include/ezrassor_dashboard/help_window.h
@@ -0,0 +1,12 @@
+// Written by Tiger Sachse.
+#ifndef HELP_WINDOW_HEADER
+#define HELP_WINDOW_HEADER
+#include <QWidget>
+#include "ui_help_window.h"
+
+class HelpWindow : public QWidget, public Ui_helpWindow {
+    Q_OBJECT
+    public:
+        HelpWindow(QWidget *parent = NULL);
+};
+#endif
