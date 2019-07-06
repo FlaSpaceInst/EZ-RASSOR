54a6b91b3f5e412b11b90d3419c383f7779ebb9f
Ronald <ronald.marrero@outlook.com>
2019-07-06 19:04:44 -0400

    Complete separation of application into components

---
+++ controller/rn-cli.config.js
@@ -0,0 +1,7 @@
+// This tweak allows for the use of jsx files in react-native
+// More information can be found at https://github.com/facebook/react-native/pull/5233#issuecomment-382083236``
+module.exports = {
+    resolver: {
+        sourceExts: ['jsx', 'js'],
+    },
+};
