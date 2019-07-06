69560fc271842ed58841a23a05e11e2c324c9da8
Tiger Sachse <tgsachse@gmail.com>
2019-07-06 15:42:04 -0400

    finished #284, removed master URI functionality

--- packages/extras/ezrassor_dashboard/include/ezrassor_dashboard/main_window.h
+++ packages/extras/ezrassor_dashboard/include/ezrassor_dashboard/main_window.h
@@ -3,24 +3,11 @@
 #ifndef MAIN_WINDOW_HEADER
 #define MAIN_WINDOW_HEADER
 #include <QWidget>
-#include <QCloseEvent>
 #include "ui_main_window.h"
 
 class MainWindow : public QWidget, public Ui_mainWindow {
     Q_OBJECT
     public:
         MainWindow(QWidget *parent = NULL);
-        void closeEvent(QCloseEvent*);
-    
-    public slots:
-        void finalizeConnection(void);
-        void finalizeDisconnection(void);
-        void handleConnectButtonPress(void);
-        void recoverFromConnectionFailure(void);
-
-    signals:
-        void closed(void);
-        void disconnectionRequested(void);
-        void connectionRequested(const std::string&);
 };
 #endif
