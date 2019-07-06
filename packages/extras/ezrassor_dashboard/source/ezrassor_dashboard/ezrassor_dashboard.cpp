0d1846bbd70ec610dcbcb7e6e05fb4fcda59368c
Tiger Sachse <tiger@sachse.io>
2019-07-06 17:07:12 -0400

    show help menu if cant immediately connect to ROS master

--- packages/extras/ezrassor_dashboard/source/ezrassor_dashboard/ezrassor_dashboard.cpp
+++ packages/extras/ezrassor_dashboard/source/ezrassor_dashboard/ezrassor_dashboard.cpp
@@ -3,6 +3,7 @@
 // Written by Tiger Sachse.
 #include <QApplication>
 #include "main_window.h"
+#include "help_window.h"
 #include "topic_translator.h"
 
 // The main entry point of the EZ-RASSOR Dashboard.
@@ -11,35 +12,45 @@ int main(int argumentCount, char** argumentVector) {
     // Initialize the application and its main window, and its topic translator.
     QApplication application(argumentCount, argumentVector);
     MainWindow mainWindow;
-    TopicTranslator topicTranslator(
-        argumentCount,
-        argumentVector,
-        "dashboard",
-        100,
-        "memory_usage",
-        "cpu_usage",
-        "battery_remaining"
-    );
+    HelpWindow helpWindow;
+    TopicTranslator* topicTranslator;
 
-    // Show relevant data from ROS topics in the GUI.
-    application.connect(
-        &topicTranslator,
-        SIGNAL(processorDataReceived(int)),
-        mainWindow.processorUsageBar,
-        SLOT(setValue(int))
-    );
-    application.connect(
-        &topicTranslator,
-        SIGNAL(memoryDataReceived(int)),
-        mainWindow.memoryUsageBar,
-        SLOT(setValue(int))
-    );
-    application.connect(
-        &topicTranslator,
-        SIGNAL(batteryDataReceived(int)),
-        mainWindow.batteryRemainingBar,
-        SLOT(setValue(int))
-    );
+    try {
+        topicTranslator = new TopicTranslator(
+            argumentCount,
+            argumentVector,
+            "dashboard",
+            100,
+            "memory_usage",
+            "cpu_usage",
+            "battery_remaining"
+        );
+
+        // Show relevant data from ROS topics in the GUI.
+        application.connect(
+            topicTranslator,
+            SIGNAL(processorDataReceived(int)),
+            mainWindow.processorUsageBar,
+            SLOT(setValue(int))
+        );
+        application.connect(
+            topicTranslator,
+            SIGNAL(memoryDataReceived(int)),
+            mainWindow.memoryUsageBar,
+            SLOT(setValue(int))
+        );
+        application.connect(
+            topicTranslator,
+            SIGNAL(batteryDataReceived(int)),
+            mainWindow.batteryRemainingBar,
+            SLOT(setValue(int))
+        );
+
+        topicTranslator->start();
+    }
+    catch (int TRANSLATOR_INITIALIZATION_FAILED) {
+        helpWindow.show();
+    }
 
     // Run the application.
     mainWindow.show();
