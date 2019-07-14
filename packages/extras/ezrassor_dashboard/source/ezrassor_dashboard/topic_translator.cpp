7a20fb4eb0efe6efcdec2632ebd38cb973731a31
Tiger Sachse <tgsachse@gmail.com>
2019-07-14 17:37:33 -0400

    initial commit of IMU hookup, working on making numbers more readable

--- packages/extras/ezrassor_dashboard/source/ezrassor_dashboard/topic_translator.cpp
+++ packages/extras/ezrassor_dashboard/source/ezrassor_dashboard/topic_translator.cpp
@@ -4,6 +4,7 @@
 
 #include <QImage>
 #include <QThread>
+#include <QString>
 #include <QPixmap>
 #include "ros/ros.h"
 #include "std_msgs/Float64.h"
@@ -17,20 +18,34 @@ TopicTranslator::TopicTranslator(
     char** argumentVector,
     const std::string& nodeName,
     int queueSize,
+    const std::string& imuTopic,
     const std::string& memoryUsageTopic,
     const std::string& processorUsageTopic,
     const std::string& batteryRemainingTopic,
     const std::string& leftCameraImageTopic,
     const std::string& rightCameraImageTopic)
     : queueSize(queueSize),
+      imuTopic(imuTopic),
       memoryUsageTopic(memoryUsageTopic),
       processorUsageTopic(processorUsageTopic),
       batteryRemainingTopic(batteryRemainingTopic),
       leftCameraImageTopic(leftCameraImageTopic),
       rightCameraImageTopic(rightCameraImageTopic) {
 
+    currentMemoryPercentage = 0;
+    currentBatteryPercentage = 0;
+    currentProcessorPercentage = 0;
     currentLeftCameraImage = QPixmap();
     currentRightCameraImage = QPixmap();
+    currentXOrientation = QString();
+    currentYOrientation = QString();
+    currentZOrientation = QString();
+    currentXAngularVelocity = QString();
+    currentYAngularVelocity = QString();
+    currentZAngularVelocity = QString();
+    currentXLinearAcceleration = QString();
+    currentYLinearAcceleration = QString();
+    currentZLinearAcceleration = QString();
 
     // Attempt to contact ROS Master. If ROS Master can't be reached, throw
     // an error.
@@ -50,76 +65,95 @@ TopicTranslator::~TopicTranslator(void) {
 // Run the translator ROS node.
 void TopicTranslator::run(void) {
     ros::NodeHandle nodeHandle;
+    ros::Subscriber imuSubscriber = nodeHandle.subscribe(
+        imuTopic,
+        queueSize,
+        &TopicTranslator::saveIMUData,
+        this
+    );
     ros::Subscriber processorUsageSubscriber = nodeHandle.subscribe(
         processorUsageTopic,
         queueSize,
-        &TopicTranslator::handleProcessorData,
+        &TopicTranslator::saveProcessorData,
         this
     );
     ros::Subscriber memoryUsageSubscriber = nodeHandle.subscribe(
         memoryUsageTopic,
         queueSize,
-        &TopicTranslator::handleMemoryData,
+        &TopicTranslator::saveMemoryData,
         this
     );
     ros::Subscriber batteryRemainingSubscriber = nodeHandle.subscribe(
         batteryRemainingTopic,
         queueSize,
-        &TopicTranslator::handleBatteryData,
+        &TopicTranslator::saveBatteryData,
         this
     );
     ros::Subscriber leftCameraImageSubscriber = nodeHandle.subscribe(
         leftCameraImageTopic,
         queueSize,
-        &TopicTranslator::handleLeftCameraImage,
+        &TopicTranslator::saveLeftCameraImage,
         this
     );
     ros::Subscriber rightCameraImageSubscriber = nodeHandle.subscribe(
         rightCameraImageTopic,
         queueSize,
-        &TopicTranslator::handleRightCameraImage,
+        &TopicTranslator::saveRightCameraImage,
         this
     );
 
     ros::spin();
 }
 
-// Handle incoming IMU data from ROS.
-void TopicTranslator::handleIMUData(const sensor_msgs::Imu::ConstPtr& message) {
-    Q_EMIT imuOrientationXReceived((double) message->orientation.x);
-    Q_EMIT imuOrientationYReceived((double) message->orientation.y);
-    Q_EMIT imuOrientationZReceived((double) message->orientation.z);
-    Q_EMIT imuAngularVelocityXReceived((double) message->angular_velocity.x);
-    Q_EMIT imuAngularVelocityYReceived((double) message->angular_velocity.y);
-    Q_EMIT imuAngularVelocityZReceived((double) message->angular_velocity.z);
-    Q_EMIT imuLinearAccelerationXReceived((double) message->linear_acceleration.x);
-    Q_EMIT imuLinearAccelerationYReceived((double) message->linear_acceleration.y);
-    Q_EMIT imuLinearAccelerationZReceived((double) message->linear_acceleration.z);
+// Save incoming IMU data from ROS.
+void TopicTranslator::saveIMUData(const sensor_msgs::Imu::ConstPtr& message) {
+    currentXOrientation.setNum(message->orientation.x);
+    currentYOrientation.setNum(message->orientation.y);
+    currentZOrientation.setNum(message->orientation.z);
+    currentXAngularVelocity.setNum(message->angular_velocity.x);
+    currentYAngularVelocity.setNum(message->angular_velocity.y);
+    currentZAngularVelocity.setNum(message->angular_velocity.z);
+    currentXLinearAcceleration.setNum(message->linear_acceleration.x);
+    currentYLinearAcceleration.setNum(message->linear_acceleration.y);
+    currentZLinearAcceleration.setNum(message->linear_acceleration.z);
+
+    Q_EMIT xOrientationReceived(currentXOrientation);
+    Q_EMIT yOrientationReceived(currentYOrientation);
+    Q_EMIT zOrientationReceived(currentZOrientation);
+    Q_EMIT xAngularVelocityReceived(currentXAngularVelocity);
+    Q_EMIT yAngularVelocityReceived(currentYAngularVelocity);
+    Q_EMIT zAngularVelocityReceived(currentZAngularVelocity);
+    Q_EMIT xLinearAccelerationReceived(currentXLinearAcceleration);
+    Q_EMIT yLinearAccelerationReceived(currentYLinearAcceleration);
+    Q_EMIT zLinearAccelerationReceived(currentZLinearAcceleration);
 }
 
-// Handle incoming memory data from ROS.
-void TopicTranslator::handleMemoryData(const std_msgs::Float64::ConstPtr& message) {
-    Q_EMIT memoryDataReceived((int) message->data);
+// Save incoming memory data from ROS.
+void TopicTranslator::saveMemoryData(const std_msgs::Float64::ConstPtr& message) {
+    currentMemoryPercentage = (int) message->data;
+    Q_EMIT memoryDataReceived(currentMemoryPercentage);
 }
 
-// Handle incoming battery data from ROS.
-void TopicTranslator::handleBatteryData(const std_msgs::Float64::ConstPtr& message) {
-    Q_EMIT batteryDataReceived((int) message->data);
+// Save incoming battery data from ROS.
+void TopicTranslator::saveBatteryData(const std_msgs::Float64::ConstPtr& message) {
+    currentBatteryPercentage = (int) message->data;
+    Q_EMIT batteryDataReceived(currentBatteryPercentage);
 }
 
-// Handle incoming processor data from ROS.
-void TopicTranslator::handleProcessorData(const std_msgs::Float64::ConstPtr& message) {
-    Q_EMIT processorDataReceived((int) message->data);
+// Save incoming processor data from ROS.
+void TopicTranslator::saveProcessorData(const std_msgs::Float64::ConstPtr& message) {
+    currentProcessorPercentage = (int) message->data;
+    Q_EMIT processorDataReceived(currentProcessorPercentage);
 }
 
-// Handle incoming left camera images from ROS.
-void TopicTranslator::handleLeftCameraImage(const sensor_msgs::ImageConstPtr& message) {
+// Save incoming left camera images from ROS.
+void TopicTranslator::saveLeftCameraImage(const sensor_msgs::ImageConstPtr& message) {
     processCameraImage(message, &currentLeftCameraImage);
     Q_EMIT leftCameraImageReceived(currentLeftCameraImage);
 }
 
-// Handle incoming right camera images from ROS.
-void TopicTranslator::handleRightCameraImage(const sensor_msgs::ImageConstPtr& message) {
+// Save incoming right camera images from ROS.
+void TopicTranslator::saveRightCameraImage(const sensor_msgs::ImageConstPtr& message) {
     processCameraImage(message, &currentRightCameraImage);
     Q_EMIT rightCameraImageReceived(currentRightCameraImage);
 }
