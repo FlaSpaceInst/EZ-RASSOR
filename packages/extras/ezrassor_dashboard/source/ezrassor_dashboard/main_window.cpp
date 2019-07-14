cbd9bdc23711c71fcceed25b76843d9f8945eaf4
Tiger Sachse <tgsachse@gmail.com>
2019-07-14 14:20:11 -0400

    comment drive, cameras added to gui

--- packages/extras/ezrassor_dashboard/source/ezrassor_dashboard/main_window.cpp
+++ packages/extras/ezrassor_dashboard/source/ezrassor_dashboard/main_window.cpp
@@ -1,5 +1,6 @@
-// The MainWindow class holds all of the interfaces for the Dashboard.
+// A main window which contains all of the interfaces that make up the Dashboard.
 // Written by Tiger Sachse.
+
 #include <QWidget>
 #include <QCloseEvent>
 #include "main_window.h"
@@ -9,6 +10,7 @@ MainWindow::MainWindow(QWidget *parent) : QWidget(parent) {
     setupUi(this);
 }
 
+// When the main window closes, emit a SIGNAL.
 void MainWindow::closeEvent(QCloseEvent* closeEvent) {
     Q_EMIT closed();
     closeEvent->accept();
