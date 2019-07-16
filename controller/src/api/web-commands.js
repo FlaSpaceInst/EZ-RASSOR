cbab1347a7b51f8f96fa98c00337e6321ffb96f3
Ronald <ronald.marrero@outlook.com>
2019-07-15 22:33:45 -0400

    Finalizing separation of standard robot operations

---
+++ controller/src/api/web-commands.js
@@ -0,0 +1,21 @@
+export default class HTTP { 
+
+    static doPost(host, message) { 
+        console.log("Attempting to post: " + message);
+
+        return fetch(
+            host,
+            {
+                headers: {"Content-Type":"text/plain; charset=utf-8"},
+                method: 'POST',
+                headers:{
+                    Accept: 'application/json',
+                },
+                body: message
+            }
+        )
+        .catch((error) => {
+            console.log(error);
+        });
+    }
+}
