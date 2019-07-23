8a7684e46becda878aafc4579575a3b72b1c462f
Ronald <ronald.marrero@outlook.com>
2019-07-22 21:23:14 -0400

    Wiring up the final buttons. Only text sizing remains...

--- controller/App.js
+++ controller/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import ControllerStyle from './src/styles/controller.js';
 import ControllerScreen from './src/components/app/ControllerScreen'
 
 export default class App extends React.Component {
 
   render() {
     return(
-      <ControllerScreen Style={ControllerStyle}/>
+      <ControllerScreen />
     )
   } 
 } 
