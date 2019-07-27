8a3136ba3b3f7808fa8273adc606ae304dcf7d30
Tiger Sachse <tgsachse@gmail.com>
2019-07-26 23:49:22 -0400

    Removed pointers in main(), reordered code for safe initialization, #287

--- packages/extras/ezrassor_dashboard/source/ezrassor_dashboard/ezrassor_dashboard.cpp
+++ packages/extras/ezrassor_dashboard/source/ezrassor_dashboard/ezrassor_dashboard.cpp
@@ -11,17 +11,57 @@
 
 // The main entry point to the EZ-RASSOR Dashboard.
 int main(int argumentCount, char** argumentVector) {
+    //RvizPlugin* orientationViewer;
+    //RvizPlugin* pointCloudViewer;
+    //RvizPlugin* poseViewer;
+    /*
+    orientationViewer = new RvizPlugin(
+        mainWindow.orientationFeedWidget,
+        RvizPlugin::ORIENTATION_VIEWER,
+        "imu"
+    );*/
 
-    // Initialize the application, its windows, and its topic translator.
+    /*
+    pointCloudViewer = new RvizPlugin(
+        mainWindow.pointCloudFeedWidget,
+        RvizPlugin::POINT_CLOUD_VIEWER,
+        "points2"
+    );
+
+    poseViewer = new RvizPlugin(
+        mainWindow.poseFeedWidget,
+        RvizPlugin::POSE_VIEWER
+    );
+    */
+
     QApplication application(argumentCount, argumentVector);
-    TopicTranslator* topicTranslator;
-    RvizPlugin* orientationViewer;
-    RvizPlugin* pointCloudViewer;
-    RvizPlugin* poseViewer;
     MainWindow mainWindow;
     HelpWindow helpWindow;
-
     mainWindow.show();
+
+    // Create a topic translator. Start its thread If it initializes properly,
+    // otherwise show a help menu.
+    TopicTranslator topicTranslator(
+        argumentCount,
+        argumentVector,
+        100,
+        "ezrassor_dashboard",
+        "dashboard",
+        "imu",
+        "/rosout_agg",
+        "memory_usage",
+        "cpu_usage",
+        "battery_remaining",
+        "left/image_raw",
+        "right/image_raw",
+        "disparity"
+    );
+    if (topicTranslator.initialized()) {
+        topicTranslator.start();
+    }
+    else {
+        helpWindow.show();
+    }
 
     // If the main window closes, the whole program should quit.
     application.connect(
@@ -31,155 +71,114 @@ int main(int argumentCount, char** argumentVector) {
         SLOT(quit(void))
     );
 
-    try {
-        topicTranslator = new TopicTranslator(
-            argumentCount,
-            argumentVector,
-            100,
-            "ezrassor_dashboard",
-            "dashboard",
-            "imu",
-            "/rosout_agg",
-            "memory_usage",
-            "cpu_usage",
-            "battery_remaining",
-            "left/image_raw",
-            "right/image_raw",
-            "disparity"
-        );
+    // Connect IMU data to the GUI.
+    application.connect(
+        &topicTranslator,
+        SIGNAL(xOrientationReceived(const QString&)),
+        mainWindow.xOrientationLineEdit,
+        SLOT(setText(const QString&))
+    );
+    application.connect(
+        &topicTranslator,
+        SIGNAL(yOrientationReceived(const QString&)),
+        mainWindow.yOrientationLineEdit,
+        SLOT(setText(const QString&))
+    );
+    application.connect(
+        &topicTranslator,
+        SIGNAL(zOrientationReceived(const QString&)),
+        mainWindow.zOrientationLineEdit,
+        SLOT(setText(const QString&))
+    );
+    application.connect(
+        &topicTranslator,
+        SIGNAL(xAngularVelocityReceived(const QString&)),
+        mainWindow.xAngularVelocityLineEdit,
+        SLOT(setText(const QString&))
+    );
+    application.connect(
+        &topicTranslator,
+        SIGNAL(yAngularVelocityReceived(const QString&)),
+        mainWindow.yAngularVelocityLineEdit,
+        SLOT(setText(const QString&))
+    );
+    application.connect(
+        &topicTranslator,
+        SIGNAL(zAngularVelocityReceived(const QString&)),
+        mainWindow.zAngularVelocityLineEdit,
+        SLOT(setText(const QString&))
+    );
+    application.connect(
+        &topicTranslator,
+        SIGNAL(xLinearAccelerationReceived(const QString&)),
+        mainWindow.xLinearAccelerationLineEdit,
+        SLOT(setText(const QString&))
+    );
+    application.connect(
+        &topicTranslator,
+        SIGNAL(yLinearAccelerationReceived(const QString&)),
+        mainWindow.yLinearAccelerationLineEdit,
+        SLOT(setText(const QString&))
+    );
+    application.connect(
+        &topicTranslator,
+        SIGNAL(zLinearAccelerationReceived(const QString&)),
+        mainWindow.zLinearAccelerationLineEdit,
+        SLOT(setText(const QString&))
+    );
 
-        /*
-        orientationViewer = new RvizPlugin(
-            mainWindow.orientationFeedWidget,
-            RvizPlugin::ORIENTATION_VIEWER,
-            "imu"
-        );*/
+    // Connect log messages to the GUI.
+    application.connect(
+        &topicTranslator,
+        SIGNAL(logDataReceived(const QString&)),
+        mainWindow.rosoutTextEdit,
+        SLOT(append(const QString&))
+    );
 
-        pointCloudViewer = new RvizPlugin(
-            mainWindow.pointCloudFeedWidget,
-            RvizPlugin::POINT_CLOUD_VIEWER,
-            "points2"
-        );
+    // Connect status monitor data to the GUI.
+    application.connect(
+        &topicTranslator,
+        SIGNAL(processorDataReceived(int)),
+        mainWindow.processorUsageBar,
+        SLOT(setValue(int))
+    );
+    application.connect(
+        &topicTranslator,
+        SIGNAL(memoryDataReceived(int)),
+        mainWindow.memoryUsageBar,
+        SLOT(setValue(int))
+    );
+    application.connect(
+        &topicTranslator,
+        SIGNAL(batteryDataReceived(int)),
+        mainWindow.batteryRemainingBar,
+        SLOT(setValue(int))
+    );
 
-        poseViewer = new RvizPlugin(
-            mainWindow.poseFeedWidget,
-            RvizPlugin::POSE_VIEWER
-        );
-
-        // Show relevant data from ROS topics in the GUI.
-        application.connect(
-            topicTranslator,
-            SIGNAL(xOrientationReceived(const QString&)),
-            mainWindow.xOrientationLineEdit,
-            SLOT(setText(const QString&))
-        );
-        application.connect(
-            topicTranslator,
-            SIGNAL(yOrientationReceived(const QString&)),
-            mainWindow.yOrientationLineEdit,
-            SLOT(setText(const QString&))
-        );
-        application.connect(
-            topicTranslator,
-            SIGNAL(zOrientationReceived(const QString&)),
-            mainWindow.zOrientationLineEdit,
-            SLOT(setText(const QString&))
-        );
-        application.connect(
-            topicTranslator,
-            SIGNAL(xAngularVelocityReceived(const QString&)),
-            mainWindow.xAngularVelocityLineEdit,
-            SLOT(setText(const QString&))
-        );
-        application.connect(
-            topicTranslator,
-            SIGNAL(yAngularVelocityReceived(const QString&)),
-            mainWindow.yAngularVelocityLineEdit,
-            SLOT(setText(const QString&))
-        );
-        application.connect(
-            topicTranslator,
-            SIGNAL(zAngularVelocityReceived(const QString&)),
-            mainWindow.zAngularVelocityLineEdit,
-            SLOT(setText(const QString&))
-        );
-        application.connect(
-            topicTranslator,
-            SIGNAL(xLinearAccelerationReceived(const QString&)),
-            mainWindow.xLinearAccelerationLineEdit,
-            SLOT(setText(const QString&))
-        );
-        application.connect(
-            topicTranslator,
-            SIGNAL(yLinearAccelerationReceived(const QString&)),
-            mainWindow.yLinearAccelerationLineEdit,
-            SLOT(setText(const QString&))
-        );
-        application.connect(
-            topicTranslator,
-            SIGNAL(zLinearAccelerationReceived(const QString&)),
-            mainWindow.zLinearAccelerationLineEdit,
-            SLOT(setText(const QString&))
-        );
-        application.connect(
-            topicTranslator,
-            SIGNAL(logDataReceived(const QString&)),
-            mainWindow.rosoutTextEdit,
-            SLOT(append(const QString&))
-        );
-        application.connect(
-            topicTranslator,
-            SIGNAL(processorDataReceived(int)),
-            mainWindow.processorUsageBar,
-            SLOT(setValue(int))
-        );
-        application.connect(
-            topicTranslator,
-            SIGNAL(memoryDataReceived(int)),
-            mainWindow.memoryUsageBar,
-            SLOT(setValue(int))
-        );
-        application.connect(
-            topicTranslator,
-            SIGNAL(batteryDataReceived(int)),
-            mainWindow.batteryRemainingBar,
-            SLOT(setValue(int))
-        );
-        application.connect(
-            topicTranslator,
-            SIGNAL(leftCameraImageReceived(const QPixmap&)),
-            mainWindow.leftCameraFeedLabel,
-            SLOT(setPixmap(const QPixmap&))
-        );
-        application.connect(
-            topicTranslator,
-            SIGNAL(rightCameraImageReceived(const QPixmap&)),
-            mainWindow.rightCameraFeedLabel,
-            SLOT(setPixmap(const QPixmap&))
-        );
-        application.connect(
-            topicTranslator,
-            SIGNAL(disparityMapImageReceived(const QPixmap&)),
-            mainWindow.disparityMapFeedLabel,
-            SLOT(setPixmap(const QPixmap&))
-        );
-
-        topicTranslator->start();
-    }
-    catch (int ROS_MASTER_UNREACHABLE) {
-        helpWindow.show();
-    }
-    catch (int WHITELIST_FILE_MISSING) {
-        helpWindow.show();
-    }
+    // Connect camera feeds to the GUI.
+    application.connect(
+        &topicTranslator,
+        SIGNAL(leftCameraImageReceived(const QPixmap&)),
+        mainWindow.leftCameraFeedLabel,
+        SLOT(setPixmap(const QPixmap&))
+    );
+    application.connect(
+        &topicTranslator,
+        SIGNAL(rightCameraImageReceived(const QPixmap&)),
+        mainWindow.rightCameraFeedLabel,
+        SLOT(setPixmap(const QPixmap&))
+    );
+    application.connect(
+        &topicTranslator,
+        SIGNAL(disparityMapImageReceived(const QPixmap&)),
+        mainWindow.disparityMapFeedLabel,
+        SLOT(setPixmap(const QPixmap&))
+    );
 
     // Run the application.
-    int exitCode = application.exec();
+    return application.exec();
 
     //delete orientationViewer;
-    delete pointCloudViewer;
-    delete poseViewer;
-    delete topicTranslator;
-
-    return exitCode;
+    //delete pointCloudViewer;
+    //delete poseViewer;
 }
