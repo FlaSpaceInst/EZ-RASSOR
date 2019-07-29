c7e0bb4c784a5c6d0a543f3c39570d63b3d7ad78
Ronald <ronald.marrero@outlook.com>
2019-07-28 22:16:39 -0400

    Removing relative paths from app

--- controller/App.js
+++ controller/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ControllerScreen from './src/components/app/ControllerScreen'
+import ControllerScreen from 'ezrassor-app/src/components/app/ControllerScreen'
 
 export default class App extends React.Component {
 
