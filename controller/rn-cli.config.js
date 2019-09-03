c301f58581bbeaeab5917ac76478a1fe63a05fec
CSharpRon <ronald.marrero@outlook.com>
2019-09-03 17:38:09 -0400

    Updates the Expo project for the mobile application. Closes #292

--- controller/rn-cli.config.js
+++
@@ -1,7 +0,0 @@
-// This tweak allows for the use of jsx files in react-native
-// More information can be found at https://github.com/facebook/react-native/pull/5233#issuecomment-382083236``
-module.exports = {
-    resolver: {
-        sourceExts: ['jsx', 'js'],
-    },
-};
