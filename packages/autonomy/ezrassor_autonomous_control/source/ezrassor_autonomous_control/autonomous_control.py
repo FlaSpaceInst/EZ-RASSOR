868fc9bbaf7c380c2d06f5c6fac9b551d573d118
Jordan <jalbury@knights.ucf.edu>
2019-12-23 00:09:59 -0500

    removed stereo odometer

--- packages/autonomy/ezrassor_autonomous_control/source/ezrassor_autonomous_control/autonomous_control.py
+++ packages/autonomy/ezrassor_autonomous_control/source/ezrassor_autonomous_control/autonomous_control.py
@@ -3,7 +3,6 @@ import sys
 
 from std_msgs.msg import Int8, Int16, String
 from geometry_msgs.msg import Point
-from nav_msgs.msg import Odometry
 from gazebo_msgs.msg import LinkStates
 from sensor_msgs.msg import JointState
 from sensor_msgs.msg import Imu
@@ -14,23 +13,22 @@ import ai_objects as obj
 import auto_functions as af
 import utility_functions as uf
 
-def on_start_up(target_x, target_y, movement_topic, front_arm_topic, 
+def on_start_up(target_x, target_y, movement_topic, front_arm_topic,
                 back_arm_topic, front_drum_topic, back_drum_topic,
-                max_linear_velocity=1, max_angular_velocity=1, 
-                real_odometry=False):
+                max_linear_velocity=1, max_angular_velocity=1):
     """ Initialization Function  """
 
-    # ROS Node Init Parameters 
+    # ROS Node Init Parameters
     rospy.init_node('autonomous_control')
-    
+
     #Create Utility Objects
     world_state = obj.WorldState()
-    ros_util = obj.ROSUtility(movement_topic, 
+    ros_util = obj.ROSUtility(movement_topic,
                               front_arm_topic,
-                              back_arm_topic, 
+                              back_arm_topic,
                               front_drum_topic,
-                              back_drum_topic, 
-                              max_linear_velocity, 
+                              back_drum_topic,
+                              max_linear_velocity,
                               max_angular_velocity)
 
     target_location = Point()
@@ -46,38 +44,31 @@ def on_start_up(target_x, target_y, movement_topic, front_arm_topic,
     world_state.dig_site = temp
 
     # Setup Subscriber Callbacks
-    if real_odometry:
-        # This topic will be changed to represent whatever
-        # topic the odometry data is being published to
-        rospy.Subscriber('stereo_odometer/odometry', 
-                         Odometry, 
-                         world_state.odometryCallBack)
-    else:
-        rospy.Subscriber('/gazebo/link_states', 
-                         LinkStates, 
+    rospy.Subscriber('/gazebo/link_states',
+                         LinkStates,
                          world_state.simStateCallBack)
-    
-    rospy.Subscriber('imu', 
-                     Imu, 
+
+    rospy.Subscriber('imu',
+                     Imu,
                      world_state.imuCallBack)
-    rospy.Subscriber('joint_states', 
-                     JointState, 
+    rospy.Subscriber('joint_states',
+                     JointState,
                      world_state.jointCallBack)
-    rospy.Subscriber('obstacle_detect', 
-                     Int8, 
+    rospy.Subscriber('obstacle_detect',
+                     Int8,
                      world_state.visionCallBack)
-    rospy.Subscriber('autonomous_toggles', 
-                     Int8, 
+    rospy.Subscriber('autonomous_toggles',
+                     Int8,
                      ros_util.autoCommandCallBack)
-    
+
     rospy.loginfo('Autonomous control initialized.')
 
     autonomous_control_loop(world_state, ros_util)
 
 
 def full_autonomy(world_state, ros_util):
-    """ Full Autonomy Loop Function """ 
-    
+    """ Full Autonomy Loop Function """
+
     rospy.loginfo('Full autonomy activated.')
 
     while(ros_util.auto_function_command == 16):
@@ -86,21 +77,21 @@ def full_autonomy(world_state, ros_util):
             break
         af.auto_dig(world_state, ros_util, 7)
         if ros_util.auto_function_command != 16:
-            break       
+            break
         af.auto_dock(world_state, ros_util)
         if ros_util.auto_function_command != 16:
-            break       
+            break
         af.auto_dump(world_state, ros_util, 4)
         world_state.target_location.x = world_state.dig_site.x
         world_state.target_location.y = world_state.dig_site.y
-    
+
     world_state.target_location.x = world_state.dig_site.x
     world_state.target_location.y = world_state.dig_site.y
-    
+
 
 def autonomous_control_loop(world_state, ros_util):
     """ Control Auto Functions based on auto_function_command input. """
-    
+
     while(True):
         while ros_util.auto_function_command == 0 or ros_util.auto_function_command == 32:
             ros_util.publish_actions('stop', 0, 0, 0, 0)
@@ -121,9 +112,7 @@ def autonomous_control_loop(world_state, ros_util):
         else:
             rospy.loginfo('Invalid auto-function request: %s.',
                           str(ros_util.auto_function_command))
-        
+
         ros_util.auto_function_command = 0
         ros_util.publish_actions('stop', 0, 0, 0, 0)
         ros_util.control_pub.publish(False)
-
-
