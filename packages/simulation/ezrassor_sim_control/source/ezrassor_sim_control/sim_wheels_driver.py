1f4d220488bf64ff86169f1f2ae778e37b028149
blicogam <zzraina619@gmail.com>
2020-01-31 17:42:03 -0500

    Add Initial Wheel Odometry
    - Uses diff_drive_controller (not gazebo)
    - Need to fix the occasional jerking and split second pauses

--- packages/simulation/ezrassor_sim_control/source/ezrassor_sim_control/sim_wheels_driver.py
+++ packages/simulation/ezrassor_sim_control/source/ezrassor_sim_control/sim_wheels_driver.py
@@ -10,19 +10,28 @@ NODE = "sim_wheels_driver"
 TOPIC = "wheel_instructions"
 MAX_VELOCITY = 5
 
-pub_LF = rospy.Publisher('left_wheel_front_velocity_controller/command', Float64, queue_size = 10)
-pub_LB = rospy.Publisher('left_wheel_back_velocity_controller/command', Float64, queue_size = 10)
-pub_RF = rospy.Publisher('right_wheel_front_velocity_controller/command', Float64, queue_size = 10)
-pub_RB = rospy.Publisher('right_wheel_back_velocity_controller/command', Float64, queue_size = 10)
+#pub_LF = rospy.Publisher('left_wheel_front_velocity_controller/command', Float64, queue_size = 10)
+#pub_LB = rospy.Publisher('left_wheel_back_velocity_controller/command', Float64, queue_size = 10)
+#pub_RF = rospy.Publisher('right_wheel_front_velocity_controller/command', Float64, queue_size = 10)
+#pub_RB = rospy.Publisher('right_wheel_back_velocity_controller/command', Float64, queue_size = 10)
 
 def wheel_movement_callback(twist):
-    x = twist.linear.x
-    z = twist.angular.z
+    pub_front_wheels = rospy.Publisher('diff_drive_controller_front/cmd_vel', Twist, queue_size=10)
+    pub_back_wheels = rospy.Publisher('diff_drive_controller_back/cmd_vel', Twist, queue_size=10)
+    new_twist = Twist()
+    new_twist.linear.x = twist.linear.x * MAX_VELOCITY
+    new_twist.linear.y = twist.linear.y
+    new_twist.linear.z = twist.linear.z
+    new_twist.angular.x = twist.angular.x
+    new_twist.angular.y = twist.angular.y
+    new_twist.angular.z = twist.angular.z * MAX_VELOCITY
+    pub_back_wheels.publish(new_twist)
+    pub_front_wheels.publish(new_twist)
 
-    pub_LB.publish((x-z)*MAX_VELOCITY)
-    pub_LF.publish((x-z)*MAX_VELOCITY)
-    pub_RB.publish((x+z)*MAX_VELOCITY)
-    pub_RF.publish((x+z)*MAX_VELOCITY)
+    #pub_LB.publish((x-z)*MAX_VELOCITY)
+    #pub_LF.publish((x-z)*MAX_VELOCITY)
+    #pub_RB.publish((x+z)*MAX_VELOCITY)
+    #pub_RF.publish((x+z)*MAX_VELOCITY)
 
 
 def start_node():
