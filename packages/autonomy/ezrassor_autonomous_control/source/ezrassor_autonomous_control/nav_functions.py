79517ff9934692408b23710c57d185976c19c21a
Michael Jimenez <michaeljimenez@Knights.ucf.edu>
2020-03-02 15:32:53 -0500

    Added some optimizations to the get best angle function to allow it to run quicker. Fixed problem with the turn towards the angle if statement. Added back the wedgebug implementation when EZ-RASSOR cannot see anywhere to go in its current wedge. Iliminated checking nan values in the scan.ranges array that are not there by changing the loop bounds. Increased the buffer size to prevent EZ-RASSOR from taking small paths.

--- packages/autonomy/ezrassor_autonomous_control/source/ezrassor_autonomous_control/nav_functions.py
+++ packages/autonomy/ezrassor_autonomous_control/source/ezrassor_autonomous_control/nav_functions.py
@@ -2,13 +2,11 @@ import rospy
 import math
 from tf import transformations
 
-
 def euclidean_distance(x1, x2, y1, y2):
     """ Calculate Euclidean distance from (x1,y1) to (x2,y2). """
-    
-    return math.sqrt( (x2-x1)**2 + (y2-y1)**2 )
+    return math.sqrt((x2-x1)**2 + (y2-y1)**2)
 
-def calculate_heading(world_state, ros_util):
+def calculate_heading(world_state):
 
     # Calculate the new heading of the robot given its current location and the target location.
     y2 = world_state.target_location.y
@@ -16,51 +14,29 @@ def calculate_heading(world_state, ros_util):
     x2 = world_state.target_location.x
     x1 = world_state.positionX
 
+    # This is the x and y distances.
     dy = y2 - y1
     dx = x2 - x1
 
+    # Get the angle in radians.
     new_heading = math.atan2(dy, dx)
 
+    # Convert the angle to degrees.
     new_heading = (180 * new_heading/math.pi)
 
+    # Since gazebo only uses 0 through 360 degree, we must bound any negative angles to positive ones.
     if new_heading < 0:
         new_heading = 360 + new_heading
 
     return new_heading
 
-# def calculate_heading(world_state, target_x, target_y):
-#     # Calculate the new heading of the robot given its current location and the target location.
-#     y2 = target_y
-#     y1 = world_state.positionY
-#     x2 = target_x
-#     x1 = world_state.positionX
-#
-#     dy = y2 - y1
-#     dx = x2 - x1
-#
-#     new_heading = math.atan2(dy, dx)
-#
-#     new_heading = (180 * new_heading / math.pi)
-#
-#     if new_heading < 0:
-#         new_heading = 360 + new_heading
-#
-#     return new_heading
-
 def adjust_angle(heading, new_heading):
-
     # Adjust the angle difference to determine fastest turning direction to reach the goal.
     angle_difference = new_heading - heading
     angle_difference = (angle_difference + 180) % 360 - 180
-    
     return (math.pi * angle_difference / 180)
 
-
 def quaternion_to_yaw(pose):
-    quaternion = (pose.orientation.x, pose.orientation.y,
-                  pose.orientation.z, pose.orientation.w)
-
+    quaternion = (pose.orientation.x, pose.orientation.y,pose.orientation.z, pose.orientation.w)
     euler = transformations.euler_from_quaternion(quaternion)
-    
     return euler[2]
-
