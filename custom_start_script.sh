5069c4f041099390c09c11bb1931fe39f07725a6
wchinny <winnchintip@gmail.com>
2020-04-11 21:46:29 -0400

    some swarm logic tweaks

--- custom_start_script.sh
+++ custom_start_script.sh
@@ -1,2 +1,2 @@
 # just for personal use for testing and ease of launching
-roslaunch ezrassor_launcher swarm_simulation.launch robot_count:="1" spawn_x_coords:="3 -3 8 -6" spawn_y_coords:="0 4 3 3" digsite_x_coords:="8 -5 11 6" digsite_y_coords:="11 -8 -7 -7" lander_coords:="0 0"
+roslaunch ezrassor_launcher swarm_simulation.launch robot_count:="3" spawn_x_coords:="3 -3 8 -6" spawn_y_coords:="0 4 3 3" digsite_x_coords:="7 -5 14 6" digsite_y_coords:="10 -8 -5 -7" lander_coords:="0 0" enable_cameras:="False"
