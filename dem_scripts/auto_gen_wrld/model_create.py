27a7c784a33fb986919c227afd7bc899fea44824
blicogam <zzraina619@gmail.com>
2020-04-27 11:37:17 -0400

    Integrate Bug Fix to auto_gen_wrld and start README conversion to rst
    - Removed extra files
    - cool.rst is to be future README.rst
    
    TODO:
    - Update README to reflect bug fix and formatting
    - Configure to use auto read size of image
    - Allow for user input for position, squish factor, author, author email, description
    - Clean up and comment code for all of dem_scripts
    - Integrate with run_programs.sh or modify auto_gen_wrld.sh to take certain commands

--- dem_scripts/auto_gen_wrld/model_create.py
+++ dem_scripts/auto_gen_wrld/model_create.py
@@ -2,20 +2,20 @@ from lxml import etree
 import sys
 
 def replace_ref(tag, model_name, dem_name):
-        first = True
-        tag_text = tag.text
-        tag_text = tag_text.split("/")
-        tag_text[-1] = dem_name
-        tag_text[2] = model_name
-        new_tag_text = ""
-        for i in tag_text:
-            if first:
-                new_tag_text += i
-                first = False
-            else:
-                new_tag_text += "/" + i
-        print(new_tag_text)
-        tag.text = new_tag_text
+    first = True
+    tag_text = tag.text
+    tag_text = tag_text.split("/")
+    tag_text[-1] = dem_name
+    tag_text[2] = model_name
+    new_tag_text = ""
+    for i in tag_text:
+        if first:
+            new_tag_text += i
+            first = False
+        else:
+            new_tag_text += "/" + i
+    print(new_tag_text)
+    tag.text = new_tag_text
 
 def replace_diff_mod(tag, model_name):
     first = True
@@ -35,24 +35,27 @@ def replace_diff_mod(tag, model_name):
 def replace_top_model_name(tag, model_name):
     tag.attrib['name'] = model_name
 
-def main():
-    path_to_file = sys.argv[1]
-    model_name = sys.argv[2]
-    dem_img_file = sys.argv[3]
+def model_trav(path_to_file, model_name, dem_name):
     tree = etree.parse(path_to_file)
     top_tag = tree.xpath("//sdf/model")[0]
     collision_uri_tag = tree.xpath("//sdf/model/link/collision/geometry/heightmap/uri")[0]
     visual_uri_tag = tree.xpath("//sdf/model/link/visual/geometry/heightmap/uri")[0]
     visual_tex_tag = tree.xpath("//sdf/model/link/visual/geometry/heightmap/texture/diffuse")[0]
     print("Attempt to replace collision uri")
-    replace_ref(collision_uri_tag, model_name, dem_img_file)
+    replace_ref(collision_uri_tag, model_name, dem_name)
     print("Attempt to replace visual uri")
-    replace_ref(visual_uri_tag, model_name, dem_img_file)
+    replace_ref(visual_uri_tag, model_name, dem_name)
     print("Attempt to replace texture model uri")
     replace_diff_mod(visual_tex_tag, model_name)
     print("Attempt to replace top model name")
     replace_top_model_name(top_tag, model_name)
     tree.write(path_to_file)
+
+def main():
+    path_to_model = sys.argv[1]
+    model_name = sys.argv[2]
+    dem_img_file = sys.argv[3]
+    model_trav(path_to_model, model_name, dem_img_file)
 
 if __name__ == "__main__":
     main()
