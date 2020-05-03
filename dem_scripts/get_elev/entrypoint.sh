95aa1194dc7a38b4ba148ef95473c7f83888dedb
blicogam <zzraina619@gmail.com>
2020-05-03 11:41:50 -0400

    Shell scripts POSIX compliance + generated folders when program run
    - The other folders are important to keep
    - auto_move/queue_packs contains models for park_ranger_test_world
    and tile_test; Need better movement of models to ~/.gazebo/models

--- dem_scripts/get_elev/entrypoint.sh
+++ dem_scripts/get_elev/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Add local user
 # Either use the LOCAL_USER_ID if passed in at runtime or
@@ -7,7 +7,7 @@
 USER_ID=${LOCAL_USER_ID:-9001}
 
 echo "Starting with UID : $USER_ID"
-useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
+useradd --shell /bin/sh -u "$USER_ID" -o -c "" -m user
 export HOME=/home/user
 
 exec /usr/local/bin/gosu user "$@"
