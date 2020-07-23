7ba50e66b2bf986ee60bb5da3fa973d753949a48
CSharpRon <ronald.marrero@outlook.com>
2020-07-23 16:33:05 -0400

    Restoring imports; adding flake8 ignore for __init__.py

---
+++ packages/autonomy/ezrassor_autonomous_control/source/ezrassor_autonomous_control/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+
+import autonomous_control
+import ai_objects
+import nav_functions
+import utility_functions
+import obstacle_detection
+import auto_functions
+import park_ranger
--- packages/communication/ezrassor_joy_translator/source/ezrassor_joy_translator/__init__.py
+++ packages/communication/ezrassor_joy_translator/source/ezrassor_joy_translator/__init__.py
@@ -1,4 +1,7 @@
+# flake8: noqa
+
 """Initialize this directory as a Python module.
 
 Written by Tiger Sachse.
 """
+import joy_translator
