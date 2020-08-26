2afa864597a776648b094577454859bc14060f61
Tiger Sachse <tgsachse@gmail.com>
2020-08-26 12:18:10 -0700

    Update linting configuration to cover more files automatically.

--- packages/communication/ezrassor_topic_switch/source/ezrassor_topic_switch/__init__.py
+++ packages/communication/ezrassor_topic_switch/source/ezrassor_topic_switch/__init__.py
@@ -1,5 +1,3 @@
-# flake8: noqa
-
 """Initialize this directory as a Python module.
 
 Written by Tiger Sachse.
--- packages/simulation/ezrassor_sim_control/source/ezrassor_sim_control/__init__.py
+++ packages/simulation/ezrassor_sim_control/source/ezrassor_sim_control/__init__.py
@@ -1,5 +1,3 @@
-# flake8: noqa
-
 import sim_arms_driver
 import sim_drums_driver
 import sim_wheels_driver
